b595f49b1b3e348bb8cacefeb7f1d56567ddc382
lake_hu <huhengshuai@xiaomi.com>
2022-12-20 13:43:45 +0800

    feat: incentive list ok

--- mock/incentive.js
+++ mock/incentive.js
@@ -6,20 +6,22 @@ export default [
       return {
         code: 200,
         msg: "success",
-        data: [
-          {
-            incentivePlanId: 0,
-            projectId: 0,
-            percentage: 10,
-            target: 0,
-            status: 0,
-            effectiveDate: "2022-12-20",
-            createUserId: 0,
-            granted: 33000,
-            total: 1200000,
-            name: 'Dev Team TIP',
-          },
-        ],
+        data: '1'.repeat(10).split('1').map((_,idx) =>{
+          return (
+              {
+                incentivePlanId: idx,
+                projectId: idx,
+                percentage: 10 + idx,
+                target: 0,
+                status: 0,
+                effectiveDate: "2022-12-20"+idx,
+                createUserId: 0,
+                granted: 33000,
+                total: 1200000,
+                name: 'Dev Team TIP',
+              }
+          )
+        } ) ,
       };
     },
   },
--- src/pages/incentive/PlanList.jsx
+++ src/pages/incentive/PlanList.jsx
@@ -4,15 +4,15 @@ import DeleteButton from "../../partials/actions/DeleteButton";
 import IncentivesTable from "../../partials/incentives/IncentivesTable";
 import PaginationClassic from "../../components/PaginationClassic";
 import IncentiveLayout from "./Layout";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { useRequest } from "ahooks";
 import { getIncentiveList } from "@/api/incentive";
 
 function PlanList() {
   const [selectedItems, setSelectedItems] = useState([]);
   const { data: tipList = [] } = useRequest(getIncentiveList);
-
-  console.log(tipList);
+  const [tipIndex, setTipIndex] =  useState(0);
+  const previewSize = 5;
+  const maxIndex = Math.floor(tipList.length/previewSize);
 
   const handleSelectedItems = (selectedItems) => {
     setSelectedItems([...selectedItems]);
@@ -30,38 +30,63 @@ function PlanList() {
             </h1>
 
             <div className="pb-14 pl-12 flex">
-              <div className="w-[148px] h-[98px] flex-none shadow-c1 border rounded-[10px] mr-11">
+              <NavLink to={"/incentive/create"} className="w-[148px] h-[98px] flex-none shadow-c1 border rounded-[10px] mr-11">
                 1
-              </div>
-              <div className="flex-auto">
-                <Swiper spaceBetween={45} slidesPerView="auto">
-                  <SwiperSlide>
-                    {tipList.map((tip) => {
-                      return (
-                        <NavLink to={`/incentive/${tip.projectId}`} key={tip.projectId}>
-                          <div className="w-[148px] h-[98px] shadow-c2 border rounded-[10px] mr-11 relative">
-                            <div className="text-base text-[#3A4353] pt-3.5 pl-1.5">
-                              <p>{tip.name}</p>
-                              <p>{tip.effectiveDate}</p>
-                            </div>
+              </NavLink>
 
-                            <div className="absolute inset-x-0 h-1.5 overflow-hidden bottom-5 bg-[#CBD5E1]">
-                              <div className="h-1.5	bg-[#475569]" style={{width: tip.percentage + '%'}}/>
-                            </div>
-
-                            <div className="inset-x-0 absolute bottom-0 origin-left	scale-50 text-[#1E293B]">
-                                Granted {tip.granted}
-                            </div>
-                            <div className="inset-x-0 absolute bottom-0 origin-right text-right	scale-50 text-[#1E293B]">
-                                Total: {tip.total}
-                            </div>
-                          </div>
-                        </NavLink>
-                      );
-                    })}
-                  </SwiperSlide>
-                </Swiper>
+              <div className="w-[22px]  flex-none  cursor-pointer" 
+                onClick={() => {
+                setTipIndex(v => {
+                  return Math.max(0, v - 1)
+                })
+              }}>
+                左边箭头
               </div>
+
+              <div className="flex-auto h-[98px] flex">
+                {tipList.slice(tipIndex*previewSize, (tipIndex+1)*previewSize).map((tip) => {
+                  return (
+                    <NavLink
+                      key={tip.projectId}
+                      to={`/incentive/${tip.projectId}`}
+                      className="mr-11"
+                    >
+                      <div className="w-[148px] h-[98px] shadow-c2 border rounded-[10px] relative">
+                        <div className="text-base text-[#3A4353] pt-3.5 pl-1.5">
+                          <p>{tip.name}</p>
+                          <p>{tip.effectiveDate}</p>
+                        </div>
+
+                        <div className="absolute inset-x-0 h-1.5 overflow-hidden bottom-5 bg-[#CBD5E1]">
+                          <div
+                            className="h-1.5	bg-[#475569]"
+                            style={{ width: tip.percentage + "%" }}
+                          />
+                        </div>
+
+                        <div className="inset-x-0 absolute bottom-0 origin-left	scale-50 text-[#1E293B]">
+                          Granted {tip.granted}
+                        </div>
+                        <div className="inset-x-0 absolute bottom-0 origin-right text-right	scale-50 text-[#1E293B]">
+                          Total: {tip.total}
+                        </div>
+                      </div>
+                    </NavLink>
+                  );
+                })}
+              </div>
+
+              <div className="w-[22px]  flex-none  cursor-pointer"
+              // disable,tip
+                onClick={() => {
+                  setTipIndex(v => {
+                    return Math.min(maxIndex, v + 1)
+                  })
+                }}
+              >
+                右边箭头
+              </div>
+
             </div>
           </div>
         </div>
