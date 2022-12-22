99b1f0d460d3d7e7c2c850ef124067c4c8dc5f73
lake_hu <huhengshuai@xiaomi.com>
2022-12-22 11:18:55 +0800

    feat: temp

--- src/pages/incentive/PlanList.jsx
+++ src/pages/incentive/PlanList.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import DeleteButton from "../../partials/actions/DeleteButton";
 import IncentivesTable from "../../partials/incentives/IncentivesTable";
 import PaginationClassic from "../../components/PaginationClassic";
 import IncentiveLayout from "./Layout";
 import { useRequest } from "ahooks";
 import { getIncentiveList } from "@/api/incentive";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { PlusOutlined } from '@ant-design/icons'
 
 function PlanList() {
   const [selectedItems, setSelectedItems] = useState([]);
   const { data: tipList = [] } = useRequest(getIncentiveList);
-  const [tipIndex, setTipIndex] =  useState(0);
-  const previewSize = 5;
-  const maxIndex = Math.floor(tipList.length/previewSize);
 
   const handleSelectedItems = (selectedItems) => {
     setSelectedItems([...selectedItems]);
@@ -29,64 +30,61 @@ function PlanList() {
               Token Incentive Plans
             </h1>
 
-            <div className="pb-14 pl-12 flex">
-              <NavLink to={"/incentive/create"} className="w-[148px] h-[98px] flex-none shadow-c1 border rounded-[10px] mr-11">
-                1
-              </NavLink>
-
-              <div className="w-[22px]  flex-none  cursor-pointer" 
-                onClick={() => {
-                setTipIndex(v => {
-                  return Math.max(0, v - 1)
-                })
-              }}>
-                左边箭头
-              </div>
-
-              <div className="flex-auto h-[98px] flex">
-                {tipList.slice(tipIndex*previewSize, (tipIndex+1)*previewSize).map((tip) => {
-                  return (
-                    <NavLink
-                      key={tip.projectId}
-                      to={`/incentive/${tip.projectId}`}
-                      className="mr-11"
-                    >
-                      <div className="w-[148px] h-[98px] shadow-c2 border rounded-[10px] relative">
-                        <div className="text-base text-[#3A4353] pt-3.5 pl-1.5">
-                          <p>{tip.name}</p>
-                          <p>{tip.effectiveDate}</p>
-                        </div>
-
-                        <div className="absolute inset-x-0 h-1.5 overflow-hidden bottom-5 bg-[#CBD5E1]">
-                          <div
-                            className="h-1.5	bg-[#475569]"
-                            style={{ width: tip.percentage + "%" }}
-                          />
-                        </div>
-
-                        <div className="inset-x-0 absolute bottom-0 origin-left	scale-50 text-[#1E293B]">
-                          Granted {tip.granted}
-                        </div>
-                        <div className="inset-x-0 absolute bottom-0 origin-right text-right	scale-50 text-[#1E293B]">
-                          Total: {tip.total}
-                        </div>
-                      </div>
-                    </NavLink>
-                  );
-                })}
-              </div>
-
-              <div className="w-[22px]  flex-none  cursor-pointer"
-              // disable,tip
-                onClick={() => {
-                  setTipIndex(v => {
-                    return Math.min(maxIndex, v + 1)
-                  })
+            <div
+              className="px-8 relative"
+              style={{ "--swiper-navigation-size": "24px" }}
+            >
+              <div className="swiper-button-next"></div>
+              <div className="swiper-button-prev"></div>
+              <Swiper
+                modules={[Navigation]}
+                style={{ height: 110 }}
+                spaceBetween={45}
+                slidesPerView={6}
+                navigation={{
+                  nextEl: ".swiper-button-next",
+                  prevEl: ".swiper-button-prev",
                 }}
               >
-                右边箭头
-              </div>
+                <SwiperSlide>
+                  <NavLink to={"/incentive/create"}>
+                    <div className="w-[148px] h-[98px] shadow-c1 border rounded-[10px] flex justify-center align-middle">
+                      <PlusOutlined />
+                    </div>
+                  </NavLink>
+                </SwiperSlide>
+                {tipList.map((tip) => {
+                  return (
+                    <SwiperSlide key={tip.projectId}>
+                      <NavLink
+                        to={`/incentive/${tip.projectId}`}
+                        className="mr-11"
+                      >
+                        <div className="w-[148px] h-[98px] shadow-c2 border rounded-[10px] relative">
+                          <div className="text-base text-[#3A4353] pt-3.5 pl-1.5">
+                            <p>{tip.name}</p>
+                            <p>{tip.effectiveDate}</p>
+                          </div>
 
+                          <div className="absolute inset-x-0 h-1.5 overflow-hidden bottom-5 bg-[#CBD5E1]">
+                            <div
+                              className="h-1.5	bg-[#475569]"
+                              style={{ width: tip.percentage + "%" }}
+                            />
+                          </div>
+
+                          <div className="inset-x-0 absolute bottom-0 origin-left	scale-50 text-[#1E293B]">
+                            Granted {tip.granted}
+                          </div>
+                          <div className="inset-x-0 absolute bottom-0 origin-right text-right	scale-50 text-[#1E293B]">
+                            Total: {tip.total}
+                          </div>
+                        </div>
+                      </NavLink>
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
             </div>
           </div>
         </div>
