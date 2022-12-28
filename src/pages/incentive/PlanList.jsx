077d1c5de0768520018c40a3f994220de5c7766c
lake_hu <huhengshuai@xiaomi.com>
2022-12-28 21:30:25 +0800

    feat: all grant list

--- src/pages/incentive/PlanList.jsx
+++ src/pages/incentive/PlanList.jsx
@@ -1,127 +1,153 @@
-import React, { useState , useEffect} from "react";
-import { NavLink } from "react-router-dom";
-import IncentivesTable from "../../partials/incentives/IncentivesTable";
-import PaginationClassic from "../../components/PaginationClassic";
+import React, {useState, useEffect} from "react";
+import {NavLink} from "react-router-dom";
 import IncentiveLayout from "./Layout";
-import { getIncentiveList } from "@/api/incentive";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
-import { useSelector } from "react-redux";
+import {getIncentiveList, getTipGrantList} from "@/api/incentive";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation} from "swiper";
+import {useSelector} from "react-redux";
 import "swiper/css";
 import "swiper/css/navigation";
-import { PlusOutlined } from '@ant-design/icons'
+import {PlusOutlined} from '@ant-design/icons'
+import GrantTable from './GrantTable'
 
 function PlanList() {
-  const userStore = useSelector((state) => state.user);
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [tipList, updateTipList] =  useState([]);
+    const userStore = useSelector((state) => state.user);
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [tipList, updateTipList] = useState([]);
+    const [grantList, updateGrantList] = useState([])
 
-  useEffect(() => {
-    let ignore = false;
+    useEffect(() => {
+        let ignore = false;
 
-    async function getList (){
-      const id = userStore?.projects?.[0]?.projectId
-      if(!id) return;
-      const data = await getIncentiveList(userStore?.projects?.[0]?.projectId)
-      if (!ignore && Array.isArray(data)){
-        console.log('TIP list->', data)
-        updateTipList(data)
-      } 
-    }
+        async function getList() {
+            const id = userStore?.projects?.[0]?.projectId
+            if (!id) return;
+            const data = await getIncentiveList(userStore?.projects?.[0]?.projectId)
+            if (!ignore && Array.isArray(data)) {
+                updateTipList(data)
+            }
+        }
 
-    getList()
+        getList()
 
-    return () => {
-      ignore = true; 
+        return () => {
+            ignore = true;
+        };
+    }, [userStore?.projects?.[0]?.projectId]);
+
+    useEffect(() => {
+        let ignore = false;
+        async function getGrantList() {
+            for (let tipIdx in tipList) {
+                const tip = tipList[tipIdx]
+                const list = await getTipGrantList(tip.incentivePlanId) || []
+                updateGrantList(existedList => {
+//                    if(existedList)
+                  if (!existedList.find(item => item?.grant?.grantId === list?.[0]?.grant?.grantId) && !ignore) {
+                        return [...list, ...existedList]
+                    }
+
+                    return existedList
+                })
+            }
+        }
+
+        getGrantList()
+
+        return () => {
+            ignore = true;
+        };
+    }, [tipList]);
+
+    const handleSelectedItems = (selectedItems) => {
+        setSelectedItems([...selectedItems]);
     };
-  }, [userStore?.projects?.[0]?.projectId]); 
 
-  const handleSelectedItems = (selectedItems) => {
-    setSelectedItems([...selectedItems]);
-  };
+    return (
+        <IncentiveLayout>
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+                {/* Page header */}
+                <div className="sm:flex sm:justify-between sm:items-center mb-8">
+                    {/* Left: Title */}
+                    <div className="mb-4 sm:mb-0 w-full">
+                        <h1 className="pb-6 text-3xl md:text-3xl text-slate-800 font-bold">
+                            Token Incentive Plans
+                        </h1>
 
-  return (
-    <IncentiveLayout>
-      <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-        {/* Page header */}
-        <div className="sm:flex sm:justify-between sm:items-center mb-8">
-          {/* Left: Title */}
-          <div className="mb-4 sm:mb-0 w-full">
-            <h1 className="pb-6 text-3xl md:text-3xl text-slate-800 font-bold">
-              Token Incentive Plans
-            </h1>
+                        <div
+                            className="px-8 relative"
+                            style={{"--swiper-navigation-size": "24px"}}
+                        >
+                            <div className="swiper-button-next"></div>
+                            <div className="swiper-button-prev"></div>
+                            <Swiper
+                                modules={[Navigation]}
+                                style={{height: 110}}
+                                spaceBetween={45}
+                                slidesPerView={6}
+                                navigation={{
+                                    nextEl: ".swiper-button-next",
+                                    prevEl: ".swiper-button-prev",
+                                }}
+                            >
+                                <SwiperSlide>
+                                    <NavLink to={"/incentive/create"}>
+                                        <div
+                                            className="w-[148px] h-[98px] shadow-c1 border rounded-[10px] flex justify-center align-middle">
+                                            <PlusOutlined/>
+                                        </div>
+                                    </NavLink>
+                                </SwiperSlide>
+                                {tipList.map((tip) => {
+                                    return (
+                                        <SwiperSlide key={tip.incentivePlanId}>
+                                            <NavLink
+                                                to={`/incentive/${tip.incentivePlanId}`}
+                                                className="mr-11"
+                                            >
+                                                <div
+                                                    className="w-[148px] h-[98px] shadow-c2 border rounded-[10px] relative">
+                                                    <div className="text-base text-[#3A4353] pt-3.5 pl-1.5">
+                                                        <p>{tip.incentivePlanName}</p>
+                                                    </div>
 
-            <div
-              className="px-8 relative"
-              style={{ "--swiper-navigation-size": "24px" }}
-            >
-              <div className="swiper-button-next"></div>
-              <div className="swiper-button-prev"></div>
-              <Swiper
-                modules={[Navigation]}
-                style={{ height: 110 }}
-                spaceBetween={45}
-                slidesPerView={6}
-                navigation={{
-                  nextEl: ".swiper-button-next",
-                  prevEl: ".swiper-button-prev",
-                }}
-              >
-                <SwiperSlide>
-                  <NavLink to={"/incentive/create"}>
-                    <div className="w-[148px] h-[98px] shadow-c1 border rounded-[10px] flex justify-center align-middle">
-                      <PlusOutlined />
-                    </div>
-                  </NavLink>
-                </SwiperSlide>
-                {tipList.map((tip) => {
-                  return (
-                    <SwiperSlide key={tip.incentivePlanId}>
-                      <NavLink
-                        to={`/incentive/${tip.incentivePlanId}`}
-                        className="mr-11"
-                      >
-                        <div className="w-[148px] h-[98px] shadow-c2 border rounded-[10px] relative">
-                          <div className="text-base text-[#3A4353] pt-3.5 pl-1.5">
-                            <p>{tip.incentivePlanName}</p>
-                          </div>
+                                                    <div
+                                                        className="absolute inset-x-0 h-1.5 overflow-hidden bottom-5 bg-[#CBD5E1]">
+                                                        <div
+                                                            className="h-1.5	bg-[#475569]"
+                                                            style={{width: tip.percentage + "%"}}
+                                                        />
+                                                    </div>
 
-                          <div className="absolute inset-x-0 h-1.5 overflow-hidden bottom-5 bg-[#CBD5E1]">
-                            <div
-                              className="h-1.5	bg-[#475569]"
-                              style={{ width: tip.percentage + "%" }}
-                            />
-                          </div>
-
-                          <div className="inset-x-0 absolute bottom-0 origin-left	scale-50 text-[#1E293B]">
-                            Granted {tip.grantedTokenNum}
-                          </div>
-                          <div className="inset-x-0 absolute bottom-0 origin-right text-right	scale-50 text-[#1E293B]">
-                            Total: {tip.totalTokenNum}
-                          </div>
+                                                    <div
+                                                        className="inset-x-0 absolute bottom-0 origin-left	scale-50 text-[#1E293B]">
+                                                        Granted {tip.grantedTokenNum}
+                                                    </div>
+                                                    <div
+                                                        className="inset-x-0 absolute bottom-0 origin-right text-right	scale-50 text-[#1E293B]">
+                                                        Total: {tip.totalTokenNum}
+                                                    </div>
+                                                </div>
+                                            </NavLink>
+                                        </SwiperSlide>
+                                    );
+                                })}
+                            </Swiper>
                         </div>
-                      </NavLink>
-                    </SwiperSlide>
-                  );
-                })}
-              </Swiper>
+                    </div>
+                </div>
+
+                {/* Table */}
+                <h1 className="pb-6 text-3xl md:text-3xl text-slate-800 font-bold">
+                    Grants
+                </h1>
+                <GrantTable list={grantList} title={() => <h2 className="font-bold text-base	inline">All The Grants <p
+                    className="font-light	 inline">{grantList.length}</p></h2>
+                }/>
+
             </div>
-          </div>
-        </div>
-
-        {/* Table */}
-        <h1 className="pb-6 text-3xl md:text-3xl text-slate-800 font-bold">
-          Grants
-        </h1>
-        <IncentivesTable selectedItems={handleSelectedItems} />
-
-        {/* Pagination */}
-        <div className="mt-8">
-          <PaginationClassic />
-        </div>
-      </div>
-    </IncentiveLayout>
-  );
+        </IncentiveLayout>
+    );
 }
 
 export default PlanList;
