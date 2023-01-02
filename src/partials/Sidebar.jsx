384afb64b4c031c763c213f8938ec11d9d2c3cf4
lake_hu <huhengshuai@xiaomi.com>
2023-01-02 20:19:39 +0800

    feat: add dashboard page

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -155,19 +155,12 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   {(handleClick, open) => {
                     return (
                       <React.Fragment>
-                        <a
-                          href="#0"
-                          className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
-                            (pathname === "/" ||
-                              pathname.includes("dashboard")) &&
-                            "hover:text-slate-200"
-                          }`}
-                          onClick={(e) => {
-                            e.preventDefault();
-                            sidebarExpanded
-                              ? handleClick()
-                              : setSidebarExpanded(true);
-                          }}
+                        <NavLink
+                          to="/"
+                          className={({ isActive }) =>
+                            "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
+                            (isActive ? "!text-indigo-500" : "")
+                          }
                         >
                           <div className="flex items-center justify-between">
                             <div className="flex items-center">
@@ -204,65 +197,8 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                 Dashboard
                               </span>
                             </div>
-                            {/* Icon */}
-                            <div className="flex shrink-0 ml-2">
-                              <svg
-                                className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${
-                                  open && "rotate-180"
-                                }`}
-                                viewBox="0 0 12 12"
-                              >
-                                <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
-                              </svg>
-                            </div>
                           </div>
-                        </a>
-                        <div className="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                          <ul className={`pl-9 mt-1 ${!open && "hidden"}`}>
-                            <li className="mb-1 last:mb-0">
-                              <NavLink
-                                end
-                                to="/"
-                                className={({ isActive }) =>
-                                  "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
-                                  (isActive ? "!text-indigo-500" : "")
-                                }
-                              >
-                                <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                  Main
-                                </span>
-                              </NavLink>
-                            </li>
-                            <li className="mb-1 last:mb-0">
-                              <NavLink
-                                end
-                                to="/dashboard/analytics"
-                                className={({ isActive }) =>
-                                  "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
-                                  (isActive ? "!text-indigo-500" : "")
-                                }
-                              >
-                                <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                  Analytics
-                                </span>
-                              </NavLink>
-                            </li>
-                            <li className="mb-1 last:mb-0">
-                              <NavLink
-                                end
-                                to="/dashboard/fintech"
-                                className={({ isActive }) =>
-                                  "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
-                                  (isActive ? "!text-indigo-500" : "")
-                                }
-                              >
-                                <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                  Fintech
-                                </span>
-                              </NavLink>
-                            </li>
-                          </ul>
-                        </div>
+                        </NavLink>
                       </React.Fragment>
                     );
                   }}
