a929c0cf3fee07ec9ec78f57893c4f7a401923e5
lake_hu <huhengshuai@xiaomi.com>
2023-01-18 19:02:11 +0800

    单位

--- src/components/local/KV2.jsx
+++ src/components/local/KV2.jsx
@@ -6,7 +6,7 @@ const { Paragraph } = Typography;
 const KV = ({ label, value, clx }) => {
   return (
     <div className={`${clx || ""} shrink-0 mb-1 grid grid-cols-2	gap-x-6`}>
-      <p className="text-sm text-[#94A3B8] mr-6 text-right">{label}</p>
+      <p className="text-sm text-[#94A3B8] text-right">{label}</p>
       <h3 className="text-base	font-semibold	text-[#1E293B] text-left">
         <Paragraph
           ellipsis={{
