b515abae6710b319e69da3768c755523e5a3052a
lake_hu <huhengshuai@xiaomi.com>
2023-02-11 23:43:42 +0800

    m header

--- src/components/DropdownProfile.jsx
+++ src/components/DropdownProfile.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { DownOutlined } from "@ant-design/icons";
-import { Popover } from "antd";
+import { Popover, Drawer } from "antd";
 import { useAccount, useEnsName } from "wagmi";
 import { shortAddress } from "@/utils/const";
 import clsx from "clsx";
@@ -13,34 +12,19 @@ import closeIcon from "@/images/icon/close.svg";
 import useCurrentProjectId from "@/hooks/useCurrentProjectId";
 import useProjects from "@/hooks/useProjects";
 import useCurrentProject from "@/hooks/useCurrentProject";
+import { useResponsive } from "ahooks";
 
 function DropdownProfile({ align }) {
   const userStore = useSelector((state) => state.user.user);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const dispatch = useDispatch();
   const currentProjectId = useCurrentProjectId();
-  const trigger = useRef(null);
-  const dropdown = useRef(null);
+
   const { address } = useAccount();
   const { data: ensName } = useEnsName({ address });
   const projects = useProjects();
   const currentProject = useCurrentProject();
-
-  // close on click outside
-  useEffect(() => {
-    const clickHandler = ({ target }) => {
-      if (!dropdown.current) return;
-      if (
-        !dropdownOpen ||
-        dropdown.current.contains(target) ||
-        trigger.current.contains(target)
-      )
-        return;
-      setDropdownOpen(false);
-    };
-    document.addEventListener("click", clickHandler);
-    return () => document.removeEventListener("click", clickHandler);
-  });
+  const { pc } = useResponsive();
 
   // close if the esc key is pressed
   useEffect(() => {
@@ -58,12 +42,12 @@ function DropdownProfile({ align }) {
     setDropdownOpen(false);
   };
 
-  const content = (
-    <div className="-mx-3" ref={dropdown}>
-      <div className="relative flex items-center justify-end w-full px-6 pb-2.5 border-b">
+  const Content = () => (
+    <div className="-mx-6 lg:-mx-3">
+      <div className="relative flex items-center justify-between lg:justify-end w-full px-6 pb-2.5 lg:border-b">
         <div className="flex">
           <NetWork />
-          <span className="ml-1.5 mr-12 block text-[18px] leading-[20px] text-[#333]">
+          <span className="ml-1.5 mr-12 block text-[18px] lg:leading-[20px] text-[#333]">
             {ensName || shortAddress(userStore?.mainWallet)}
           </span>
         </div>
@@ -87,7 +71,7 @@ function DropdownProfile({ align }) {
           </svg>
         </p>
       </div>
-      <div className="divide-y max-h-[160px] overflow-y-auto">
+      <div className="divide-y max-h-[160px] text-center overflow-y-auto">
         {projects.map((project) => {
           const isSelected = project.projectId === currentProjectId;
           return (
@@ -101,7 +85,7 @@ function DropdownProfile({ align }) {
                   : "hover:font-semibold text-[#999] bg-white cursor-pointer"
               )}
             >
-              <div className="flex items-center justify-start mr-2">
+              <div className="flex items-center justify-center mr-2 lg:justify-start">
                 {project.projectName}
               </div>
             </div>
@@ -110,38 +94,67 @@ function DropdownProfile({ align }) {
       </div>
 
       <Link to="/incentive/create" onClick={() => setDropdownOpen(false)}>
-        <div className="px-3 border-t py-2 text-[#999] text-[16px] leading-[24px]">
+        <div className="px-3 border-t text-center py-2 text-[#999] text-[16px] leading-[24px]">
           New Incentive plan for a new project
         </div>
       </Link>
     </div>
   );
+  const Avator = () => (
+    <div
+      className="flex items-center cursor-pointer"
+      onClick={() => setDropdownOpen(true)}
+    >
+      <div
+        className={clsx(
+          "w-9 h-9 lg:w-8 lg:h-8 rounded-full  flex justify-center items-center mr-2 shadow-c9",
+          dropdownOpen ? "bg-[#0049FF]" : "bg-[#e6e6e6]"
+        )}
+      >
+        <img src={userStore?.avatar} className="lg:w-5 w-[25px]" />
+      </div>
+      <span
+        className={clsx(
+          "hidden lg:block text-[14px] leading-[22px]",
+          clsx(dropdownOpen ? "text-[#0049FF]" : "text-[#333]")
+        )}
+      >
+        {ensName || shortAddress(userStore?.mainWallet)}
+      </span>
+    </div>
+  );
   return (
     <div className="relative inline-flex">
-      <span className="text-[14px] leading-[32px] mr-4">{currentProject?.projectName}</span>
-      <Popover open={dropdownOpen} content={content} placement="bottomRight">
-        <div
-          className="flex items-center cursor-pointer"
-          onClick={() => setDropdownOpen(true)}
+      <span className="text-[14px] leading-[32px] mr-4 text-[#666]">
+        {currentProject?.projectName}
+      </span>
+
+      {pc ? (
+        <Popover
+          open={dropdownOpen}
+          content={<Content />}
+          placement="bottomRight"
         >
-          <div
-            className={clsx(
-              "w-9 h-9 lg:w-8 lg:h-8 rounded-full  flex justify-center items-center mr-2 shadow-c9",
-              dropdownOpen ? "bg-[#0049FF]" : "bg-[#e6e6e6]"
-            )}
+          <Avator />
+        </Popover>
+      ) : (
+        <>
+          <Avator />
+          <Drawer
+            placement="bottom"
+            closable={false}
+            open={dropdownOpen}
+            contentWrapperStyle={{
+              height: "50vh",
+              borderRadius: "24px 24px 0px 0px",
+              overflow: "hidden",
+            }}
+            onClose={() => setDropdownOpen(false)}
           >
-            <img src={userStore?.avatar} className="lg:w-5 w-[25px]" />
-          </div>
-          <span
-            className={clsx(
-              "hidden lg:block text-[14px] leading-[22px]",
-              clsx(dropdownOpen ? "text-[#0049FF]" : "text-[#333]")
-            )}
-          >
-            {ensName || shortAddress(userStore?.mainWallet)}
-          </span>
-        </div>
-      </Popover>
+            <Content />
+          </Drawer>
+        </>
+      )}
     </div>
   );
 }
