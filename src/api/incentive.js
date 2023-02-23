1576797fba7e1a91be31f102818dd4157a46e848
gamma916 <garfeildma@yandex.com>
2023-02-23 18:38:52 +0800

    add testing env

--- src/api/incentive.js
+++ src/api/incentive.js
@@ -1,6 +1,6 @@
 import request from "./request";
 
-export const host = "https://api.tbook.com"
+export const host = import.meta.env.VITE_API_HOST;
 
 export const getUserInfo = async function(){
     return request(`${host}/info`)
