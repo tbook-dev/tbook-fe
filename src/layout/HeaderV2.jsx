01efdeb23ce30651b3bb1c43b3dc6b69b3004219
lake_hu <huhengshuai@xiaomi.com>
2023-02-27 10:09:02 +0800

    fix build

--- src/layout/HeaderV2.jsx
+++ src/layout/HeaderV2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UserMenu from "@/components/UserMenu";
+import UserMenu from "@/components/userMenu";
 import Logo from "../components/icon/Logo";
 
 function Header() {
