c35ffeb05952367411053d6deccab19eed0d0830
lake_hu <huhengshuai@xiaomi.com>
2023-03-02 18:08:22 +0800

    t

--- src/pages/sign/Grant/NotConnect.jsx
+++ src/pages/sign/Grant/NotConnect.jsx
@@ -10,8 +10,8 @@ export default function () {
       className="absolute inset-0 flex items-center bg-center bg-cover"
       style={{ backgroundImage: `url(${grante403})` }}
     >
-      <div className="lg:w-[578px] lg:ml-[120px]">
-        <h2 className="font-extrabold text-white text-cwh4 lg:mb-6">
+      <div className="w-[80vw] text-center lg:text-left mx-auto lg:w-[578px] lg:ml-[120px]">
+        <h2 className="inline-block mb-5 font-extrabold text-white text-ch1 lg:text-cwh4 lg:mb-6">
           <span className="mr-4 text-colorful1">Connect</span>the wallet to
           check your grants.
         </h2>
