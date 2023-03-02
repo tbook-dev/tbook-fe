280fe2c8b0cbadb10b9d2d30a4b00fcbc28dd808
lake_hu <huhengshuai@xiaomi.com>
2023-03-02 14:51:38 +0800

    feat: 手机端非首页均加上返回首页按钮

---
+++ src/components/back/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import backIcon from "@/images/icon/h5-back.svg";
+
+// 56+12=68px
+export default function ({ link = "/" }) {
+  return (
+    <Link to={link} className="fixed left-4 top-[68px]">
+      <button>
+        <img src={backIcon} className="w-7 h-7"/>
+      </button>
+    </Link>
+  );
+}
