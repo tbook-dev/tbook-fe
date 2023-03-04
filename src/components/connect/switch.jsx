4c87da70bcaa421fd2946f48b5ba123fbb791930
lake_hu <huhengshuai@xiaomi.com>
2023-03-04 15:01:29 +0800

    fix: 87

--- src/components/connect/switch.jsx
+++ src/components/connect/switch.jsx
@@ -46,17 +46,17 @@ export default function ({
 
   const Lay = () => (
     <div className="-mx-6 lg:-mx-3 w-auto lg:w-[300px]">
-      <div className="text-[18px] leading-[26px] text-center pb-4 pt-1">
-        <p className="text-[#333]">Choose the network</p>
+      <div className="pt-1 pb-4 text-center text-c7">
+        <p className="font-medium text-black">Choose the network</p>
       </div>
       <div>
         {chains.map((v) => (
           <div
             className={clsx(
-              "flex items-center justify-start py-2 pl-6  hover:text-[#666] cursor-pointer",
+              "flex items-center text-c6 justify-start py-2 pl-6 ",
               currentId === v.evmChainId
-                ? "text-[#0049FF] bg-[#ECF1FF]"
-                : "text-[#999] bg-white hover:bg-white"
+                ? " bg-cw1 dark:text-black cursor-not-allowed hover:opacity-70"
+                : "text-[#999] bg-white cursor-pointer"
             )}
             onClick={() => {
               setOpenLay(false);
