b0b906a7394d632fe1bd3cec3d14167452886936
lake_hu <huhengshuai@xiaomi.com>
2023-03-04 15:35:03 +0800

    fix: 切换项目h5

--- src/components/userMenu/H5Drawer.jsx
+++ src/components/userMenu/H5Drawer.jsx
@@ -4,7 +4,7 @@ import { Drawer } from "antd";
 import clsx from "clsx";
 import { useDispatch } from "react-redux";
 import { setCurrentProjectId } from "@/store/user";
-import switchIcon from "@/images/icon/pop-switch.svg";
+import switchIcon from "@/images/icon/switch-plan.png";
 import useCurrentProjectId from "@/hooks/useCurrentProjectId";
 import useProjects from "@/hooks/useProjects";
 import useCurrentProject from "@/hooks/useCurrentProject";
@@ -20,16 +20,18 @@ export default function ({ open, setOpen }) {
   const projects = useProjects();
   const currentProject = useCurrentProject();
   const navigate = useNavigate();
+  const [menuStep, setStep] = useState(0);
 
   const hanldeChangeProject = function (project) {
     if (currentProjectId === project.projectId) return;
     dispatch(setCurrentProjectId(project.projectId));
     setOpen(false);
-    navigate('/')
+    setStep(0);
+    navigate("/");
   };
 
-  const Content = () => (
-    <div className="lg:-mx-3 lg:w-[330px]">
+  const InitMenu = () => (
+    <div className="mx-6 my-6">
       <Info />
 
       <div className="text-c9 text-center pt-[30px] pb-2.5">
@@ -38,52 +40,53 @@ export default function ({ open, setOpen }) {
           className="flex items-center justify-center font-medium text-[#333] cursor-pointer"
           onClick={(evt) => {
             evt?.stopPropagation();
-            setExpaned(!expanded);
+            setExpaned(true);
+            setStep(1);
           }}
         >
-          <span className="text-colorful1">{currentProject?.projectName}</span>
-          <img
-            className={clsx(
-              "w-5 h-5 ml-1",
-              expanded && "rotate-180"
-            )}
-            src={switchIcon}
-          />
+          <span className="font-medium text-black">{currentProject?.projectName}</span>
+          <img className={clsx("w-9 h-9 ml-1", expanded && "rotate-180")} src={switchIcon} />
         </div>
       </div>
+    </div>
+  );
 
-      {expanded && (
-        <>
-          <div className="max-h-[160px] text-[16px] leading-[24px] text-center overflow-y-auto">
-            {projects.map((project) => {
-              const isSelected = project.projectId === currentProjectId;
-              return (
-                <div
-                  key={project.projectId}
-                  onClick={() => hanldeChangeProject(project)}
-                  className={clsx(
-                    "px-6 py-2",
-                    isSelected
-                      ? "text-[#333] bg-[#f2f2f2] font-medium cursor-not-allowed"
-                      : "hover:text-[#333] text-[#999] bg-white cursor-pointer"
-                  )}
-                >
-                  <div className="flex items-center justify-center mr-2">
-                    {project.projectName}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-
-          <Link to="/create/project" onClick={() => setOpen(false)}>
-            <div className="px-3 text-center py-2 mt-4 mb-3 bg-[#f2f2f2] mx-2.5 rounded-lg text-[#999] hover:text-[#333] text-[16px] leading-[24px]">
-              <PlusOutlined style={{ color: "#0049FF", marginRight: 8 }} />
-              Incentivize a new project
+  const SwitchProject = () => (
+    <>
+      <h2 className="text-black text-c7 h-[86px] leading-[86px] text-center">Switch to another project</h2>
+      <div className="max-h-[160px] text-[16px] leading-[24px] text-center overflow-y-auto">
+        {projects.map((project) => {
+          const isSelected = project.projectId === currentProjectId;
+          return (
+            <div
+              key={project.projectId}
+              onClick={() => hanldeChangeProject(project)}
+              className={clsx(
+                "px-6 h-10 flex items-center justify-center",
+                isSelected
+                  ? "text-black bg-cw1 font-medium cursor-not-allowed"
+                  : "text-[#666] bg-white cursor-pointer"
+              )}
+            >
+              {project.projectName}
             </div>
-          </Link>
-        </>
-      )}
+          );
+        })}
+      </div>
+
+      <Link to="/create/project" onClick={() => setOpen(false)}>
+        <div className="px-3 py-2 mt-4 mb-3 text-center text-c7 text-[#666]">
+          <PlusOutlined style={{ color: "#69D0E5", marginRight: 8 }} />
+          Incentivize a new project
+        </div>
+      </Link>
+    </>
+  );
+
+  const Content = () => (
+    <div className="-mx-6 -my-6">
+      {menuStep === 0 && <InitMenu />}
+      {menuStep === 1 && <SwitchProject />}
     </div>
   );
 
@@ -94,6 +97,7 @@ export default function ({ open, setOpen }) {
         setOpen={() => {
           setOpen(true);
           setExpaned(false);
+          setStep(0);
         }}
         open={open}
       />
@@ -106,7 +110,9 @@ export default function ({ open, setOpen }) {
           borderRadius: "24px 24px 0px 0px",
           overflow: "hidden",
         }}
-        onClose={() => setOpen(false)}
+        onClose={() => {
+          setOpen(false);
+        }}
       >
         <Content />
       </Drawer>
