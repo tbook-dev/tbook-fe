e9e3b4aa28d5251980cbcf565b5fc40b044b20fd
lake_hu <huhengshuai@xiaomi.com>
2023-03-10 11:43:22 +0800

    product assets

---
+++ app/home/src/partials/product/conf.js
@@ -0,0 +1,29 @@
+import dashboard from "@/images/product/dashboard.png";
+import approach from "@/images/product/approach.png";
+import templates from "@/images/product/templates.png";
+import privacy from "@/images/product/privacy.png";
+
+const list = [
+  {
+    src: templates,
+    title: "Numerous Proven Templates",
+    desc: "By analyzing  numbers of token incentive plans of the top web3 projects in each category, we promptly give you the  most suitable solution",
+  },
+  {
+    src: approach,
+    title: "Flexible Incentive Approach",
+    desc: "Numerous possibilities that can be created by combining various incentive scenarios and sets, just like Lego ",
+  },
+  {
+    src: dashboard,
+    title: "All-in-one data dashboard",
+    desc: "Easily trace the details of all token grants and readily identify the most noteworthy data",
+  },
+  {
+    src: privacy,
+    title: "Ultimate Privacy Protection",
+    desc: "By encrypting the details of each grant, we provide an additional layer of protection against unauthorized access or tampering",
+  },
+];
+
+export default list;
