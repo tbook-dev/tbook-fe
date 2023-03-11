ac971f7c52b19a0a6f2519f5d38c6ea55aa1e65a
lake_hu <huhengshuai@xiaomi.com>
2023-03-11 21:22:53 +0800

    fix: 头图修改

--- app/home/src/partials/banner/index.jsx
+++ app/home/src/partials/banner/index.jsx
@@ -8,7 +8,7 @@ const { appLink, myLink } = conf;
 export default function () {
   return (
     <section className="relative mt-[44px] lg:mt-[126px] lg:h-[600px] lg:mb-[92px] mb-10">
-      <div className="bx">
+      <div className="relative bx">
         <div className="w-[240px] lg:w-[427px] mb-2 lg:mb-6 mx-auto lg:ml-0">
           <div className="lg:pt-[126px] lg:mb-4">
             <div className="h-[80px] lg:h-[150px]">
@@ -38,8 +38,10 @@ export default function () {
             </Button>
           </a>
         </div>
+
+        <img src={banner} className="-mr-4 hidden lg:block lg:absolute lg:top-0 lg:right-0 lg:h-[600px]" />
       </div>
-      <div className="-mr-4">
+      <div className="-mr-4 lg:hidden">
         <img src={banner} className="relative -mr-4 lg:absolute lg:top-0 lg:right-0 lg:h-[600px]" />
       </div>
     </section>
