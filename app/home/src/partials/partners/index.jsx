4a74b9df3404038d6c833d9fee758129d1d3391c
lake_hu <huhengshuai@xiaomi.com>
2023-03-12 20:20:01 +0800

    动画连续

--- app/home/src/partials/partners/index.jsx
+++ app/home/src/partials/partners/index.jsx
@@ -1,19 +1,22 @@
-import { useMemo  } from 'react';
+import { useMemo } from "react";
 import { list1, list2 } from "./conf";
 import { useResponsive } from "ahooks";
 
 export default function Partners() {
   const { pc } = useResponsive();
-  const w = useMemo(
-    () => {
-      return {
-        pc: (246 + 80) * list1.length - 80,
-        m: (123 + 40) * list1.length - 40,
-      };
-    },
-    []
-  );
-  console.log(w)
+  const w1 = useMemo(() => {
+    return {
+      pc: (246 + 80) * list1.length,
+      m: (123 + 40) * list1.length,
+    };
+  }, []);
+  const w2 = useMemo(() => {
+    return {
+      pc: (246 + 80) * list2.length,
+      m: (123 + 40) * list2.length,
+    };
+  }, []);
+  console.log({ list1, list2 });
 
   return (
     <div className="mb-10 lg:mb-[144px]">
@@ -25,22 +28,47 @@ export default function Partners() {
       </div>
 
       <div className="overflow-hidden bx">
-        {/* (123 + 80)* list1.length - 80  */}
-        <div
-          className="flex mb-8 flex-nowrap lg:mb-16 loop-left hover:animation-paused"
-          style={{ width: pc ? w.pc : w.m }}
-        >
-          {list1.map((v) => (
-            <img key={v.src} src={v.src} className="flex-none w-[123px] h-[48px] lg:w-[246px] lg:h-[96px] mr-10 lg:mr-20" />
-          ))}
-          {/* {list1.map((v) => (
-            <img key={v.src} src={v.src} className="w-[123px] h-[48px] lg:w-[246px] lg:h-[96px] mr-10 lg:mr-20" />
-          ))} */}
+        <div className="flex" style={{ width: 2 * (pc ? w1.pc : w1.m) }}>
+          <div
+            className="flex mb-8 flex-nowrap lg:mb-16 loop-left hover:animation-paused"
+            style={{ width: pc ? w1.pc : w1.m }}
+          >
+            {list1.map((v) => (
+              <img
+                key={v.src}
+                src={v.src}
+                className="flex-none w-[123px] h-[48px] lg:w-[246px] lg:h-[96px] mr-10 lg:mr-20"
+              />
+            ))}
+            {list1.map((v) => (
+              <img
+                key={v.src}
+                src={v.src}
+                className="flex-none w-[123px] h-[48px] lg:w-[246px] lg:h-[96px] mr-10 lg:mr-20"
+              />
+            ))}
+          </div>
         </div>
-        <div className="flex mb-8 flex-nowrap lg:mb-16 loop-right hover:animation-paused">
-          {list2.map((v) => (
-            <img key={v.src} src={v.src} className="w-[123px] h-[48px] lg:w-[246px] lg:h-[96px]  mr-10 lg:mr-20" />
-          ))}
+        <div className="flex" style={{ width: 2 * (pc ? w2.pc : w2.m) }}>
+          <div
+            className="flex mb-8 flex-nowrap lg:mb-16 loop-right hover:animation-paused"
+            style={{ width: pc ? w2.pc : w2.m }}
+          >
+            {list2.map((v) => (
+              <img
+                key={v.src}
+                src={v.src}
+                className="flex-none w-[123px] h-[48px] lg:w-[246px] lg:h-[96px] mr-10 lg:mr-20"
+              />
+            ))}
+            {list2.map((v) => (
+              <img
+                key={v.src}
+                src={v.src}
+                className="flex-none w-[123px] h-[48px] lg:w-[246px] lg:h-[96px] mr-10 lg:mr-20"
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
