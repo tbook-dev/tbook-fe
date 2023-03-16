6caa52e7aa9c5fbc382286ab8fd4d98cd1173924
lake_hu <huhengshuai@xiaomi.com>
2023-03-16 14:59:51 +0800

    strength

--- app/home/src/pages/about/index.jsx
+++ app/home/src/pages/about/index.jsx
@@ -1,11 +1,13 @@
 import Banner from "@/partials/about/banner";
 import Mission from "@/partials/about/mission";
+import Strength from "@/partials/about/strength";
 
 export default function About() {
   return (
     <div className="w-full text-[#202124] mb-4 lg:px-0">
       <Banner />
       <Mission />
+      <Strength />
     </div>
   );
 }
