04a524c53dd1ff654c7251d00a61bddb650edcc2
lake_hu <huhengshuai@xiaomi.com>
2023-03-16 14:42:17 +0800

    feat: mission

--- app/home/src/pages/about/index.jsx
+++ app/home/src/pages/about/index.jsx
@@ -1,9 +1,11 @@
 import Banner from "@/partials/about/banner";
+import Mission from "@/partials/about/mission";
 
 export default function About() {
   return (
     <div className="w-full text-[#202124] mb-4 lg:px-0">
       <Banner />
+      <Mission />
     </div>
   );
 }
--- app/home/src/partials/about/banner/index.jsx
+++ app/home/src/partials/about/banner/index.jsx
@@ -1,13 +1,13 @@
 import { Button } from "@tbook/ui";
 import { conf } from "@tbook/utils";
 
-import bgVideo from "./bg.mp4";
+import bgVideo from "./banner.mp4";
 const { appLink } = conf;
 
 export default function () {
   return (
-    <div className="lg:h-[836px] overflow-hidden relative">
-      <video className="relative inset-0 object-cover h-[210px] w-full lg:h-full lg:absolute" loop autoPlay>
+    <div className="lg:h-[836px] overflow-hidden relative mb-10 lg:mb-0">
+      <video className="relative inset-0 object-cover h-[210px] w-full lg:h-auto lg:absolute" loop autoPlay>
         <source src={bgVideo} type="video/mp4"></source>
       </video>
       <div className="flex justify-center ">
