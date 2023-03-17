1decaca601a663ca602eae96625ac28c241d398a
lake_hu <huhengshuai@xiaomi.com>
2023-03-17 10:33:14 +0800

    advice from ux

--- app/home/src/partials/about/strength/index.jsx
+++ app/home/src/partials/about/strength/index.jsx
@@ -3,7 +3,7 @@ export default function () {
     <div className="px-4 mb-10  lg:mb-[144px] text-center text-white">
       <div className="p-10 rounded-lg lg:p-0 bx shadow-d6 lg:rounded-none lg:shadow-none">
         <h3 className="font-medium text-colorful1 text-c14 lg:text-c13 lg:mb-3">OUR STRENGTH</h3>
-        <h2 className="mb-3 font-bold lg:mb-10 text-ch1 lg:text-cwh6">
+        <h2 className="mb-3 font-bold capitalize lg:mb-10 text-ch1 lg:text-cwh6">
           leverage flexible incentivization & unleash the full potential
         </h2>
         <p className="text-c9 lg:text-cwh1">
--- app/home/src/partials/about/support/index.jsx
+++ app/home/src/partials/about/support/index.jsx
@@ -21,7 +21,7 @@ const list = [
 export default function () {
   return (
     <div className="px-4 bx mb-10 lg:mb-[144px] lg:px-0">
-      <div className="lg:flex lg:items-center lg:h-[720px] relative rounded-lg shadow-d6 lg:rounded-none lg:shadow-none pt-5 pb-[45px] lg:py-0">
+      <div className="lg:flex lg:items-center lg:h-[720px] relative overflow-hidden rounded-lg shadow-d6 lg:rounded-none lg:shadow-none pt-5 pb-[45px] lg:py-0">
         <div className="relative z-10 text-center text-white lg:text-left">
           <h3 className="font-medium lg:mb-3 text-colorful1 lg:text-black text-c14 lg:text-c13">SUPPORT FACTS</h3>
           <h2 className="font-bold text-ch1 lg:text-cwh6 lg:w-[344px] mb-3 lg:mb-12">Growing Numbers Of Us</h2>
@@ -38,6 +38,7 @@ export default function () {
           <video className="h-full rounded-2xl" loop autoPlay preload="auto" playsInline muted>
             <source src={bgVideo} type="video/mp4"></source>
           </video>
+          <div className="absolute inset-0 bg-[rgba(0,0,0,.6)] lg:hidden" />
         </div>
       </div>
     </div>
