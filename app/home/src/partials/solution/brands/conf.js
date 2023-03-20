a89ee410bb982ba9091fa2297e922228e3317717
lake_hu <huhengshuai@xiaomi.com>
2023-03-20 10:59:38 +0800

    feat: brand images v2

--- app/home/src/partials/solution/brands/conf.js
+++ app/home/src/partials/solution/brands/conf.js
@@ -1,25 +1,25 @@
-import bonfire from "@/images/partner/bonfire.png";
-import goplus from "@/images/partner/goplus.png";
-import zavenue from "@/images/partner/zavenue.png";
-import rn from "@/images/partner/rn.png";
-import chainbase from "@/images/partner/chainbase.png";
-import art from "@/images/partner/art.png";
-import n01 from "@/images/partner/01.png";
-import bmrlab from "@/images/partner/bmrlab.png";
-import metopia from "@/images/partner/metopia.png";
+import bonfire from "@/images/parterv2/bonfire.png";
+import goplus from "@/images/parterv2/goplus.png";
+import zavenue from "@/images/parterv2/zavenue.png";
+import rn from "@/images/parterv2/rn.png";
+import chainbase from "@/images/parterv2/chainbase.png";
+import art from "@/images/parterv2/art.png";
+import n01 from "@/images/parterv2/01.png";
+import bmrlab from "@/images/parterv2/bmrlab.png";
+import metopia from "@/images/parterv2/metopia.png";
 
-import movebit from "@/images/partner/movebit.png";
-import e from "@/images/partner/e.png";
-import mask from "@/images/partner/mask.png";
-import kapbook from "@/images/partner/kapbook.png";
-import nftplay from "@/images/partner/nftplay.png";
-import bestsign from "@/images/partner/bestsign.png";
-import hippyghosts from "@/images/partner/hippyghosts.png";
-import triangle from "@/images/partner/triangle.png";
-import ambition from "@/images/partner/ambition.png";
+import movebit from "@/images/parterv2/movebit.png";
+import e from "@/images/parterv2/e.png";
+import mask from "@/images/parterv2/mask.png";
+import kapbook from "@/images/parterv2/kapbook.png";
+import nftplay from "@/images/parterv2/nftplay.png";
+import bestsign from "@/images/parterv2/bestsign.png";
+import hippyghosts from "@/images/parterv2/hippyghosts.png";
+import triangle from "@/images/parterv2/triangle.png";
+import ambition from "@/images/parterv2/ambition.png";
 
 const conf = {
-  title: "Leading Web3.0 Brands Trust Us",
+  title: "Leading Brands Trust Us",
   list: [
     {
       src: bonfire,
