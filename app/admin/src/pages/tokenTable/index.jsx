4d48531e2f3ce1a129d71a1cc23ef8498e89184a
lake_hu <huhengshuai@xiaomi.com>
2023-03-28 11:32:43 +0800

    feat: static statis number

--- app/admin/src/pages/tokenTable/index.jsx
+++ app/admin/src/pages/tokenTable/index.jsx
@@ -1,3 +1,9 @@
+import Statistics from "./statistics";
+
 export default function TokenTable() {
-  return <div className="text-white">x</div>;
+  return (
+    <div className="text-white bx">
+      <Statistics />
+    </div>
+  );
 }
---
+++ app/admin/src/pages/tokenTable/statistics/index.jsx
@@ -0,0 +1,35 @@
+import { useMemo } from "react";
+import { conf } from "@tbook/utils";
+import clsx from "clsx";
+
+const { formatDollar } = conf;
+
+export default function ({ dilutedToken = 1000000, holders = 134, totalSupply = 1000000000000 }) {
+  const list = useMemo(() => {
+    return [
+      {
+        title: "FULLY DILUTED TOKEN",
+        value: formatDollar(dilutedToken),
+      },
+      {
+        title: "HOLDERS",
+        value: formatDollar(holders),
+      },
+      {
+        title: "MAX TOTAL SUPPLY",
+        value: formatDollar(totalSupply),
+        className: "col-span-2 lg:col-span-1",
+      },
+    ];
+  }, [dilutedToken, holders, totalSupply]);
+  return (
+    <div className="grid grid-cols-2 pt-[25px] gap-3 lg:gap-[14px]  lg:pt-[58px] lg:grid-cols-3">
+      {list.map((v) => (
+        <div key={v.title} className={clsx(v.className, "py-4 px-3 rounded lg:rounded-lg shadow-d10")}>
+          <p className="mb-3 text-xs font-medium">{v.title}</p>
+          <p className="inline font-bold text-colorful1 text-cwh5">{v.value}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
