15ca7cfa2615f77084cad930c82f04aca7ea92b7
lake_hu <huhengshuai@xiaomi.com>
2023-03-29 20:53:21 +0800

    t

---
+++ app/admin/src/pages/tokenTable/template/card.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import { useHover, useResponsive } from "ahooks";
+import { useRef } from "react";
+import clsx from "clsx";
+
+export default function ({ tpl }) {
+  const ref = useRef(null);
+  const isHovering = useHover(ref);
+  const { pc } = useResponsive();
+  console.log(pc && isHovering);
+  return (
+    <div
+      key={tpl.tplName}
+      ref={ref}
+      className="relative px-3 pt-1 pb-2 rounded-lg lg:px-4 lg:pt-2 lg:pb-6 lg:rounded-2xl lg:shadow-d6 shadow-d3"
+    >
+      <div className="h-[110px] w-[105px] mx-auto"></div>
+      <h3 className="truncate font-bold text-center lg:text-left text-c9 lg:text-cwh2 mb-1.5 lg:mb-2">{tpl.tplName}</h3>
+      <div className="mb-2 space-y-1 text-c4 lg:text-c16">
+        <div className="flex justify-between">
+          <p>Plans</p>
+          <p>{tpl.plans.length}</p>
+        </div>
+        <div className="flex justify-between">
+          <p>Holders</p>
+          <p>{tpl.holders}</p>
+        </div>
+      </div>
+
+      {pc ? (
+        <Link className={clsx(isHovering ? "block absolute left-0 right-0 bottom-0" : "hidden")}>
+          <button
+            type="button"
+            className="w-full text-c9 flex items-center justify-center h-10  font-medium leading-normal  rounded-md  dark:disabled:bg-none	dark:bg-cw1 dark:text-black shadow-d3 dark:disabled:bg-[#141414] dark:disabled:text-b-2"
+          >
+            Use as yours
+          </button>
+        </Link>
+      ) : (
+        <Link>
+          <button
+            type="button"
+            className="w-full text-c9 flex items-center justify-center h-10  font-medium leading-normal  rounded-md  dark:disabled:bg-none	dark:bg-cw1 dark:text-black shadow-d3 dark:disabled:bg-[#141414] dark:disabled:text-b-2"
+          >
+            Use as yours
+          </button>
+        </Link>
+      )}
+    </div>
+  );
+}
--- app/admin/src/pages/tokenTable/template/index.jsx
+++ app/admin/src/pages/tokenTable/template/index.jsx
@@ -2,6 +2,7 @@ import { useAsyncEffect } from "ahooks";
 import { useState } from "react";
 import { Empty } from "@tbook/ui";
 import { Link } from "react-router-dom";
+import Card from "./card";
 
 export default function Template() {
   const [tplList, setTpl] = useState([]);
@@ -66,34 +67,7 @@ export default function Template() {
       ) : (
         <div className="grid grid-cols-2 gap-2.5 lg:gap-6 lg:grid-cols-3">
           {tplList.map((tpl) => (
-            <div
-              key={tpl.tplName}
-              className="px-3 pt-1 pb-2 rounded-lg lg:px-4 lg:pt-2 lg:pb-6 lg:rounded-2xl lg:shadow-d6 shadow-d3"
-            >
-              <div className="h-[110px] w-[105px] mx-auto"></div>
-              <h3 className="truncate font-bold text-center lg:text-left text-c9 lg:text-cwh2 mb-1.5 lg:mb-2">
-                {tpl.tplName}
-              </h3>
-              <div className="mb-2 space-y-1 text-c4 lg:text-c16">
-                <div className="flex justify-between">
-                  <p>Plans</p>
-                  <p>{tpl.plans.length}</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>Holders</p>
-                  <p>{tpl.holders}</p>
-                </div>
-              </div>
-
-              <Link className="lg:hidden">
-                <button
-                  type="button"
-                  className="w-full text-c9 flex items-center justify-center h-10  font-medium leading-normal  rounded-md  dark:disabled:bg-none	dark:bg-cw1 dark:text-black shadow-d3 dark:disabled:bg-[#141414] dark:disabled:text-b-2"
-                >
-                  Use as yours
-                </button>
-              </Link>
-            </div>
+            <Card key={tpl.tplName} tpl={tpl} />
           ))}
         </div>
       )}
