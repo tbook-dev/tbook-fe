66b429ab2cfe7e4b9394834039d02af7589eb74d
lake_hu <huhengshuai@xiaomi.com>
2023-03-29 16:35:26 +0800

    template logic

--- app/admin/src/pages/tokenTable/index.jsx
+++ app/admin/src/pages/tokenTable/index.jsx
@@ -3,26 +3,41 @@ import RecordTable from "./recordTable";
 import { useCurrentProjectId, useUserInfoLoading, useProjects } from "@tbook/hooks";
 import { useAsyncEffect, useResponsive } from "ahooks";
 import { Spin } from "antd";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PlanTipNoConnect from "../incentive/planTip/NoConnect";
 import PlanTipNoProject from "../incentive/planTip/NoProject";
+import { getIncentiveList, getTipGrantList } from "@/api/incentive";
+import Template from "./template";
 
 export default function TokenTable() {
   const userLoading = useUserInfoLoading();
   const projects = useProjects();
   const authUser = useSelector((state) => state.user.authUser);
+  const projectId = useCurrentProjectId();
+  const [tipList, setTipList] = useState([]);
+  const [tipListLoading, setTiplistLoading] = useState(false);
+
   const { pc } = useResponsive();
+  useAsyncEffect(async () => {
+    if (!projectId) return;
+    setTiplistLoading(true);
+    const list = await getIncentiveList(projectId);
+    setTipList(list);
+    setTiplistLoading(false);
+  }, [projectId]);
 
   return (
     <div className="text-white bx py-[25px] lg:py-[58px]">
+      {/* section  */}
       <div className="mb-5 lg:mb-12">
-        {userLoading ? (
+        {userLoading || tipListLoading ? (
           <div className="flex items-center justify-center w-full h-full">
             <Spin />
           </div>
         ) : !authUser ? (
           <PlanTipNoConnect pc={pc} />
-        ) : projects.length === 0 ? (
+        ) : tipList.length === 0 ? (
           <PlanTipNoProject
             pc={pc}
             desc="Click to set up your incentive plan or select a template."
@@ -33,7 +48,7 @@ export default function TokenTable() {
         )}
       </div>
 
-      <RecordTable />
+      {tipList.length === 0 ? <Template /> : <RecordTable />}
     </div>
   );
 }
--- app/admin/src/pages/tokenTable/template/index.jsx
+++ app/admin/src/pages/tokenTable/template/index.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export default function Template() {
   const [tplList, setTpl] = useState([]);
-  useAsyncEffect(() => {
+  useAsyncEffect(async () => {
     setTpl([]);
   }, []);
   return <div>{tplList.length === 0 ? <div>1</div> : <div>x</div>}</div>;
