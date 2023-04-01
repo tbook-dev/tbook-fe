9024330d20fff4db3d00ce4c0b073521abf7089b
lake_hu <huhengshuai@xiaomi.com>
2023-04-01 17:23:59 +0800

    diluted token distribution

--- app/admin/src/pages/tokenTable/distributionPie/index.jsx
+++ app/admin/src/pages/tokenTable/distributionPie/index.jsx
@@ -3,10 +3,12 @@ import { useMemo, useState } from "react";
 import { useAsyncEffect } from "ahooks";
 import { getDilutedToken } from "@/api/incentive";
 import { useCurrentProjectId, useCurrentProject } from "@tbook/hooks";
-import { Spin } from "antd";
+import { Spin, Select } from "antd";
 import { round } from "lodash";
 import { conf } from "@tbook/utils";
 import Pagination from "@/components/pagination";
+import Theme from "@/components/theme";
+
 const { formatDollar, getDividePercent } = conf;
 
 export default function Pie() {
@@ -15,14 +17,28 @@ export default function Pie() {
   const projectId = useCurrentProjectId();
   const [current, setCurrent] = useState(1);
   const project = useCurrentProject();
+  const [planFilter, setPlanFilter] = useState(-1);
 
   const pageSize = 6;
   const tokenTotalAmount = project?.tokenInfo?.tokenTotalAmount || 100000000;
 
   const totalPercent = useMemo(() => {
-    const distributedTotal = dilutedToken.reduce((sum, item) => sum + item.tokenSum, 0);
+    const distributedTotal = dilutedToken
+      .filter((v) => v.tipId === planFilter)
+      .reduce((sum, item) => sum + item.tokenSum, 0);
     return getDividePercent(distributedTotal, tokenTotalAmount, 2);
-  }, [tokenTotalAmount, dilutedToken]);
+  }, [tokenTotalAmount, dilutedToken, planFilter]);
+
+  const planTypeList = useMemo(() => {
+    return dilutedToken.reduce((pre, cur) => {
+      if (pre.find((v) => v.value === cur.tipId)) {
+        return pre;
+      } else {
+        return [...pre, { value: cur.tipId, label: cur.tipName }];
+      }
+    }, []);
+  }, [dilutedToken]);
+
   useAsyncEffect(async () => {
     if (!projectId) return;
     setDilutedTokenloading(true);
@@ -39,20 +55,28 @@ export default function Pie() {
         </div>
       ) : (
         <>
-          <h2 className="font-medium text-c13 lg:mb-4">Diluted Token Distribution</h2>
+          <div className="flex justify-between lg:mb-4">
+            <h2 className="font-medium text-c13">Diluted Token Distribution</h2>
+            <Theme>
+              <Select options={planTypeList} className="w-[200px]" value={planFilter} onChange={setPlanFilter} />
+            </Theme>
+          </div>
           <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-12">
             <Liquidfill percent={totalPercent} className="self-center lg:justify-self-end justify-self-center" />
-            <div className="space-y-4 w-[342px] justify-self-start hidden lg:block">
+            <div className="space-y-4 w-[342px] justify-self-start hidden lg:block lg:h-[380px]">
               {dilutedToken.length > 0 ? (
-                dilutedToken.slice((current - 1) * pageSize, current * pageSize).map((v, idx) => (
-                  <div className="flex items-center justify-between px-4 py-3 font-medium rounded bg-b-1" key={idx}>
-                    <p className="text-c14">
-                      <span>{round(v.percentage, 2)}%</span>
-                      <span className="ml-1">{v.granteeName}</span>
-                    </p>
-                    <p className="text-c4">{formatDollar(v.tokenSum)} Tokens</p>
-                  </div>
-                ))
+                dilutedToken
+                  .filter((v) => v.tipId === planFilter)
+                  .slice((current - 1) * pageSize, current * pageSize)
+                  .map((v, idx) => (
+                    <div className="flex items-center justify-between px-4 py-3 font-medium rounded bg-b-1" key={idx}>
+                      <p className="text-c14">
+                        <span>{round(v.percentage, 2)}%</span>
+                        <span className="ml-1">{v.granteeName}</span>
+                      </p>
+                      <p className="text-c4">{formatDollar(v.tokenSum)} Tokens</p>
+                    </div>
+                  ))
               ) : (
                 <div className="px-4 py-3 font-medium text-center rounded bg-b-1">
                   <p className="text-c14">No diluted token distribution</p>
@@ -65,7 +89,7 @@ export default function Pie() {
                   showSizeChanger={false}
                   current={current}
                   pageSize={pageSize}
-                  total={dilutedToken.length}
+                  total={dilutedToken.filter((v) => v.tipId === planFilter).length}
                   onChange={setCurrent}
                 />
               </div>
