af54dfdc004c46bf4587ca293043d4737c46a734
lake_hu <huhengshuai@xiaomi.com>
2023-04-07 10:38:55 +0800

    没有权限没有登录处理

--- app/admin/src/pages/sign/Grant/NotConnect.jsx
+++ app/admin/src/pages/sign/Grant/NotConnect.jsx
@@ -3,11 +3,11 @@ import { useSignIn } from "@tbook/hooks";
 import grante403 from "@tbook/share/images/incentive/grantee403.png";
 import mgrante403 from "@tbook/share/images/incentive/mgrantee403.png";
 import { useResponsive } from "ahooks";
-import { useEffect } from 'react';
-import { user } from '@tbook/store';
+import { useEffect } from "react";
+import { user } from "@tbook/store";
 import { useDispatch } from "react-redux";
 
-const { setLessNav } =  user;
+const { setLessNav } = user;
 
 export default function () {
   const { loading, handleSignIn } = useSignIn();
@@ -17,7 +17,7 @@ export default function () {
     dispatch(setLessNav(true));
     return () => {
       dispatch(setLessNav(false));
-    }
+    };
   }, []);
 
   return (
@@ -27,12 +27,11 @@ export default function () {
     >
       <img src={mgrante403} className="block lg:hidden" />
       <div className="w-[80vw] text-center lg:text-left mx-auto lg:w-[578px] lg:ml-[120px]">
-        <h2 className="inline-block mb-5 font-extrabold text-white text-ch1 lg:text-cwh4 lg:mb-6">
-          <span className="mr-4 text-colorful1">Connect</span>the wallet to
-          check your grants.
+        <h2 className="inline-block mb-5 font-extrabold dark:text-white text-ch1 lg:text-cwh4 lg:mb-6">
+          <span className="mr-4 text-colorful1">Connect</span>the wallet to check your grants.
         </h2>
         <Button
-          className="lg:text-white lg:bg-white lg:bg-none w-[80vw] lg:w-auto"
+          className="lg:text-white lg:bg-black dark:lg:bg-white lg:bg-none w-[80vw] lg:w-auto"
           loading={loading}
           loadingColor={pc ? "#69D0E5" : "white"}
           onClick={handleSignIn}
