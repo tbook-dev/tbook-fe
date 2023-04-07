1f665b0f8050d9af5005f3e82bd9d5cb3682752f
lake_hu <huhengshuai@xiaomi.com>
2023-04-07 21:22:26 +0800

    empty

--- packages/ui/src/Empty/index.jsx
+++ packages/ui/src/Empty/index.jsx
@@ -1,9 +1,13 @@
 import emptyUrl from "@tbook/share/images/icon/empty.svg";
+import empty2 from "@tbook/share/images/icon/empty2.svg";
+
+import { useTheme } from "@tbook/hooks";
 
 export default function ({ description = "No grants", img = emptyUrl, ...props }) {
+  const theme = useTheme();
   return (
     <div className="h-[100px] flex flex-col items-center">
-      <img src={emptyUrl} className="h-full" />
+      <img src={theme === "dark" ? emptyUrl : empty2} className="h-full" />
       <span className="text-xs font-medium text-l-1 dark:text-disable lg:text-sm">{description}</span>
     </div>
   );
