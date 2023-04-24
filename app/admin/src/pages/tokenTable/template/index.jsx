366491542d4fcfeea77dfb2ec5198a5be8312795
lake_hu <huhengshuai@xiaomi.com>
2023-04-24 23:10:00 +0800

    feat: 增加分页

--- app/admin/src/pages/tokenTable/template/index.jsx
+++ app/admin/src/pages/tokenTable/template/index.jsx
@@ -4,6 +4,7 @@ import Card from "./card";
 import Select from "@/components/select/themeSelect";
 import { getTemplate, getTags } from "@/api/incentive";
 import Loading from "@/components/loading";
+import Pagination from "@/components/pagination";
 
 export default function Template({
   title = "Tokentable Templates",
@@ -12,7 +13,8 @@ export default function Template({
   const [cateGory, setCateGory] = useState([]);
   const { data: templateList = [], loading } = useRequest(() => getTemplate(cateGory), { refreshDeps: [cateGory] });
   const { data: tagList = [] } = useRequest(getTags);
-
+  const [current, setCurrent] = useState(1);
+  const pageSize = 9;
   return (
     <div className="mb-10 dark:text-white">
       <div className="mb-4 text-center">
@@ -36,11 +38,24 @@ export default function Template({
       {loading ? (
         <Loading h="h-[300px]" />
       ) : (
-        <div className="grid grid-cols-1 gap-2 lg:gap-6 lg:grid-cols-3">
-          {templateList.map((tpl) => (
-            <Card key={tpl.templateId} tpl={tpl} />
-          ))}
-        </div>
+        <>
+          <div className="grid grid-cols-1 gap-2 lg:gap-6 lg:grid-cols-3">
+            {templateList.slice((current - 1) * pageSize, current * pageSize).map((tpl) => (
+              <Card key={tpl.templateId} tpl={tpl} />
+            ))}
+          </div>
+          <div className="flex justify-end pt-4">
+            <Pagination
+              hideOnSinglePage
+              responsive
+              showSizeChanger={false}
+              current={current}
+              pageSize={pageSize}
+              total={templateList.length}
+              onChange={setCurrent}
+            />
+          </div>
+        </>
       )}
     </div>
   );
