4a4eb06750853f175a9e78c2ef9bb7611e089fbd
lake_hu <huhengshuai@xiaomi.com>
2023-04-27 22:12:57 +0800

    update

--- app/admin/src/pages/incentive/PlanList.jsx
+++ app/admin/src/pages/incentive/PlanList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useReducer, useEffect, useMemo, lazy, Suspense } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
-import { getIncentiveList, getTipGrantList } from "@/api/incentive";
+import { getIncentiveListWithGrants } from "@/api/incentive";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "swiper/css";
@@ -69,11 +69,13 @@ function PlanList() {
       return;
     }
     setGrantLoading(true);
-    const list1 = await getIncentiveList(projectId);
+    const list1 = await getIncentiveListWithGrants(projectId);
     // format
     updateTipList(list1);
     // grants
-    const list2 = await Promise.all(list1.map((tip) => getTipGrantList(tip.incentivePlanId)));
+    //const list2 = await Promise.all(list1.map((tip) => getTipGrantList(tip.incentivePlanId)));
+    const list2 = list1.map((tip) => tip.grants);
+    // console.log({ list2 });
     let activeIdx = list1.findIndex((t) => t.incentivePlanId == selectedTipId);
     if (activeIdx === -1) {
       const list2Formated = _.cloneDeep(list2)
