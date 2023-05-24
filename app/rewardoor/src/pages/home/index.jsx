4a8526da7e61ce5dd0f4e8595c1b217928a20add
lake_hu <huhengshuai@xiaomi.com>
2023-05-24 17:13:27 +0800

    t

--- app/rewardoor/src/pages/home/index.jsx
+++ app/rewardoor/src/pages/home/index.jsx
@@ -1,11 +1,27 @@
 import { Button } from 'antd'
-
+import bg from '@/images/home-bg.png'
 export default function () {
   return (
-    <div className='w-full  mb-4 px-4 lg:px-0 lg:w-[936px] mx-auto'>
-      <div className='flex justify-center'>
-        <Button className='bg-cw1'>Get Started</Button>
-        <Button>Developer Center</Button>
+    <div
+      className='w-full min-h-screen bg-[center_top_1rem]'
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className='w-[800px] mx-auto pt-[200px]'>
+        <div className='text-center mb-8'>
+          <h2 className='text-colorful1 font-bold text-6xl'>
+            Incentivize core
+          </h2>
+          <h2 className='text-colorful1 font-bold text-6xl'>
+            communities and builders
+          </h2>
+          <p className='text-white text-3xl'>
+            grant easily and optimize continuously
+          </p>
+        </div>
+        <div className='flex justify-center'>
+          <Button className='mr-6'>Get Started</Button>
+          <Button>Developer Center</Button>
+        </div>
       </div>
     </div>
   )
