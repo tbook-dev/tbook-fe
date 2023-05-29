659896be91659479639381b3f3c83cb7d3163369
lake_hu <huhengshuai@xiaomi.com>
2023-05-29 16:15:27 +0800

    feat: t

--- app/rewardoor/src/utils/upload.js
+++ app/rewardoor/src/utils/upload.js
@@ -1,9 +1,11 @@
 import { getPreSignedUrl } from '@/api/incentive'
-
 export default async function uploadFile (file) {
   const { signedUrl, accessUrl, fileName } = await getPreSignedUrl()
-  const formData = new FormData()
-  formData.append('file', file)
-  const url = await fetch(signedUrl, { method: 'PUT', body: formData })
-  console.log({ uploadS3Url, url })
+
+  const url = await fetch(signedUrl, {
+    method: 'PUT',
+    mode: 'cors',
+    body: file
+  })
+  console.log({ accessUrl, url })
 }
