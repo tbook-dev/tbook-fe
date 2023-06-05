0df03e6ece2865b91e0f52c2f0e280d82c42ad4f
lake_hu <huhengshuai@xiaomi.com>
2023-06-05 16:22:46 +0800

    feat: jira-159

--- app/rewardoor/src/modules/assets/nft.jsx
+++ app/rewardoor/src/modules/assets/nft.jsx
@@ -7,7 +7,13 @@ import { useState } from 'react'
 import { conf } from '@tbook/utils'
 import Loading from '@/components/loading'
 import clsx from 'clsx'
+import { Typography } from 'antd'
+import copyIcon from '@/images/icon/copy.svg'
+import { Icon } from '@tbook/ui'
+const { NetWork } = Icon
+
 const { shortAddress } = conf
+const { Paragraph } = Typography
 
 const title = 'Deploy NFT Contracts'
 const desc =
@@ -55,14 +61,29 @@ export default function NFT () {
                   className='rounded-button overflow-hidden h-[480px] bg-gray flex flex-col'
                 >
                   <img
-                    className='h-[319px] w-full object-contain'
+                    className='h-[319px] w-full object-cover hover:translate-y-2 hover:scale-105 transition-all transition-1000'
                     src={v.coverUrl}
                   />
                   <div className='p-6 flex flex-col justify-between flex-auto'>
                     <h2 className='font-bold text-2xl'>{v.name}</h2>
-                    <p className='font-bold text-sm'>
-                      {shortAddress(v.contract)}
-                    </p>
+                    <Paragraph
+                      className='flex items-center'
+                      copyable={{
+                        text: v.contract,
+                        icon: (
+                          <img
+                            src={copyIcon}
+                            alt='copy icon'
+                            className='w-5 h-5 object-cover'
+                          />
+                        )
+                      }}
+                    >
+                      <NetWork id={v.chainId || 1} className='mr-2' />
+                      <span className='font-bold text-sm mr-2'>
+                        {shortAddress(v.contract)}
+                      </span>
+                    </Paragraph>
                   </div>
                 </div>
               )
