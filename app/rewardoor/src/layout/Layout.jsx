49b151ed89c163b436f3f370aad33661abd6e5cc
lake <lakehu01@outlook.com>
2023-07-25 18:28:00 +0800

    update router logic

--- app/rewardoor/src/layout/Layout.jsx
+++ app/rewardoor/src/layout/Layout.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useProjects } from '@tbook/hooks'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
-const homeUrl = '/'
+
+const createUrl = '/new-project'
 
 export default function LayoutAdmin ({ children }) {
   const navigate = useNavigate()
   const authUser = useSelector(state => state.user.authUser)
   const loadingUserStatus = useSelector(state => state.user.loadingUserStatus)
   const projects = useProjects()
-
+  const isFirstRender = useRef(true)
   useEffect(() => {
     if (!loadingUserStatus && authUser) {
-      // 登录之后跳转到首页，如果project的数量大于1，跳转到首页，如果为0，跳转到创建页面
+      console.log({ projects }, 'xx')
+      // 登录之后,如果为0，跳转到创建页面
+      // 如果project的数量大于1，不做变化
+      // 如果创建了新的project，自动跳转到dashboard
       if (projects.length === 0) {
-        navigate('/create')
+        navigate(createUrl)
       } else {
-        navigate(homeUrl)
+        if (!isFirstRender.current) {
+          navigate(`/dashboard/overview`)
+        }
       }
+      isFirstRender.current = false
     }
-  }, [loadingUserStatus, authUser])
+  }, [loadingUserStatus])
+
   return (
     <div className='flex flex-col min-h-screen dark:bg-black dark:text-white bg-[#FBFDFF]'>
       <div className='relative flex-auto overflow-x-hidden overflow-y-auto'>
