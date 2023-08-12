314d85be0e21dd781d81132b89a1d2fab05f353c
lake <lakehu01@outlook.com>
2023-08-12 11:32:28 +0800

    update name

--- app/campaign/src/layout/Layout.jsx
+++ app/campaign/src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-export default function LayoutAdmin () {
+export default function Layout () {
   return (
     <div className='flex flex-col  min-h-screen dark:bg-black dark:text-white bg-[#FBFDFF]'>
       <div className='relative flex-auto overflow-x-hidden overflow-y-auto'>
