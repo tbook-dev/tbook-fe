62392940b37adac7be30dd4902dfa19fce0b4300
lake <lakehu0x@gmail.com>
2023-08-15 17:31:35 +0800

    t

--- app/rewardoor/src/hooks/useUserInfoQuery.js
+++ app/rewardoor/src/hooks/useUserInfoQuery.js
@@ -5,7 +5,8 @@ export default function useUserInfo(){
     const { data, isLoading, error,...props } = useQuery("userInfo",getUserInfo,{
         staleTime: 1000 * 60 * 10,
     });
+    const projects = data?.projects;
     const project = data?.projects?.[data?.projects?.length - 1];
     const projectId = project?.projectId;
-    return { data, isLoading, error, project, projectId, ...props};
+    return { data, isLoading, error, project, projectId,projects, ...props};
 }
