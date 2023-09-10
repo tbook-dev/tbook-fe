432bab67390a7af95f807cf0700e1e9e9ec260eb
lake <lake@krquant.com>
2023-09-10 16:26:45 +0800

    t

--- app/rewardoor/src/pages/assets/nftDetail.jsx
+++ app/rewardoor/src/pages/assets/nftDetail.jsx
@@ -1,17 +1,32 @@
 import { useParams } from 'react-router-dom'
 import NftCard from './modules/NftCard'
 import useNft from '@/hooks/queries/useNft'
+import Breadcrumb from '@/components/breadcrumb'
 
 const NftDetail = () => {
   const { nftId } = useParams()
   const { data } = useNft(nftId)
   console.log({ data })
   return (
-    <div className='flex justify-between gap-x-10'>
-      <div>table</div>
-      <div className='w-[252px]'>NftCard</div>
+    <div>
+      <Breadcrumb
+        items={[
+          {
+            title: 'Incentive Asset',
+            href: '/assets'
+          },
+          {
+            title: data?.name
+          }
+        ]}
+      />
+      <h2 className='font-bold text-5xl mb-10 text-t-1'>{data?.name}</h2>
+      <div className='flex justify-between gap-x-10'>
+        <div>table</div>
+        <div className='w-[252px]'>NftCard</div>
 
-      {/* <NftCard /> */}
+        {/* <NftCard /> */}
+      </div>
     </div>
   )
 }
