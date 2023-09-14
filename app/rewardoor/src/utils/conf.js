e6adef5c9776e1628491d1186b1632b97cff5630
lake <posuihushui@icloud.com>
2023-09-14 10:54:50 +0800

    fix: methodType

--- app/rewardoor/src/utils/conf.js
+++ app/rewardoor/src/utils/conf.js
@@ -19,20 +19,15 @@ export const rewardDistributionMethod = [
 ];
 export const projectUrlPrefix = "https://app.tbook.com" + "/";
 export const incentiveMethodList = [
-  // {
-  //   title: "Anyone who get the credentials",
-  //   desc: "Anyone who gets the credentials can claim the reward.",
-  //   value: 1,
-  // },
   {
     title: "FCFS",
     desc: "First come, first served. Whoever gets the credentials first can claim the reward first.",
-    value: 2,
+    value: 1,
   },
   {
     title: "Lucky Draw",
     desc: "A random selection of participants from those who meet the requirements.",
-    value: 3,
+    value: 2,
   },
 ];
 
