060386739d48c86fe6c1d0bd0319fbcd067bd77d
lake <lake@krquant.com>
2023-09-18 18:20:36 +0800

    new campagin detail header

--- app/campaign/src/components/textMore/index.jsx
+++ app/campaign/src/components/textMore/index.jsx
@@ -1,40 +1,43 @@
-import { useEffect, useState, useRef } from "react";
-import downIcon from "@/images/icon/down.svg";
-import clsx from "clsx";
+import { useEffect, useState, useRef } from 'react'
+import downIcon from '@/images/icon/down.svg'
+import clsx from 'clsx'
 
-export default function TextMore({ text }) {
-  const [showMore, setShowMore] = useState(false);
-  const [hasMore, setHasMore] = useState(false);
+export default function TextMore ({ text }) {
+  const [showMore, setShowMore] = useState(false)
+  const [hasMore, setHasMore] = useState(false)
 
-  const textRef = useRef(null);
+  const textRef = useRef(null)
   useEffect(() => {
     if (textRef.current) {
-      const { scrollWidth, clientWidth } = textRef.current;
+      const { scrollWidth, clientWidth } = textRef.current
       if (scrollWidth > clientWidth) {
-        setHasMore(true);
+        setHasMore(true)
       }
     }
-  }, [text]);
+  }, [text])
 
   return (
     <div
-      className="flex gap-x-5 cursor-pointer"
+      className='flex gap-x-5 cursor-pointer'
       onClick={() => {
-        setShowMore((v) => !v);
+        setShowMore(v => !v)
       }}
     >
-      <div className={clsx("text-c-9", !showMore && "truncate")} ref={textRef}>
+      <div
+        className={clsx('text-[#68696B] text-sm', !showMore && 'truncate')}
+        ref={textRef}
+      >
         {text}
       </div>
       {hasMore && (
         <img
           src={downIcon}
           className={clsx(
-            "w-4 h-4 mx-auto cursor-pointer",
-            showMore && "rotate-90"
+            'w-4 h-4 mx-auto cursor-pointer',
+            showMore && 'rotate-90'
           )}
         />
       )}
     </div>
-  );
+  )
 }
