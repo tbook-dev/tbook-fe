7917a0ea74c1adcc55507a3532ef8b77ff52f726
lake <posuihushui@icloud.com>
2023-09-30 10:40:47 +0800

    f: mock assets date

--- app/campaign/src/pages/my/modules/NFT.jsx
+++ app/campaign/src/pages/my/modules/NFT.jsx
@@ -1,3 +1,50 @@
-export default function NFT(){
-    return <div>NFT</div>
+import { shortAddress } from "@tbook/utils/lib/conf";
+const list = [
+  {
+    id: 1,
+    avator:
+      "https://pic.quanjing.com/hs/rc/QJ6174266377.jpg?x-oss-process=style/794wsy",
+    nftName: "lake-abcxx",
+    creatorAddress: "0x63xc8...bf9b",
+    createTime: "xxx",
+  },
+  {
+    id: 2,
+    avator:
+      "https://pic.quanjing.com/hs/rc/QJ6174266377.jpg?x-oss-process=style/794wsy",
+    nftName: "lake-abcxx",
+    creatorAddress: "0x63xc8...bf9b",
+    createTime: "xxx",
+  },
+];
+
+export default function NFT() {
+  return (
+    <div className="space-y-2">
+      {list.map((v) => {
+        return (
+          <div className="bg-white rounded-xl p-5 flex gap-x-6" key={v.id}>
+            <img
+              src={v.avator}
+              alt="nft"
+              className="w-[84px] h-[84px] rounded-lg object-contain object-center flex-none"
+            />
+            <div className="flex flex-col justify-between">
+              <div>
+                <h2 className="text-black text-base font-medium mb-1">
+                  {v.nftName}
+                </h2>
+                <p className="text-sm text-black">
+                  <span className="mr-1 text-[#717374]">by</span>
+                  {shortAddress(v.creatorAddress)}
+                </p>
+              </div>
+
+              <p className="text-[#717374] text-sm">{v.createTime}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- app/campaign/src/pages/my/modules/Point.jsx
+++ app/campaign/src/pages/my/modules/Point.jsx
@@ -1,3 +1,34 @@
-export default function Point(){
-    return <div>Point</div>
+import { formatDollar } from "@tbook/utils/lib/conf";
+const data = {
+  total: 3000,
+  list: [
+    {
+      num: 3,
+      date: "date",
+    },
+    {
+      num: 4,
+      date: "date",
+    },
+  ],
+};
+export default function Point() {
+  return (
+    <div className="space-y-2">
+      <div className="flex flex-col items-center justify-center text-black bg-white rounded-xl h-[124px]">
+        <div className="font-bold text-4.2xl leading-[44px] mb-1">
+          {formatDollar(data.total)}
+        </div>
+        <div className="font-medium text-base">points</div>
+      </div>
+      {data.list.map((v) => {
+        return (
+          <div className="flex items-center justify-between py-3 px-5 bg-white rounded-xl">
+            <span>{v.num}</span>
+            <span>{v.date}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
