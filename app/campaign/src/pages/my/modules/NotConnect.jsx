6c4767677769b095e9d805bbfe6882d1d9b246b7
lake <posuihushui@icloud.com>
2023-10-05 16:26:57 +0800

    r

--- app/campaign/src/pages/my/modules/NotConnect.jsx
+++ app/campaign/src/pages/my/modules/NotConnect.jsx
@@ -1,12 +1,23 @@
 import notConnectIcon from "@/images/icon/not-connect.svg";
+import { useDispatch } from "react-redux";
+import { setConnectWalletModal } from "@/store/global";
 
 const defaultText = "Log in to reveal your rewards and campaigns.";
 export default function NotConnect({ text = defaultText }) {
+  const dispath = useDispatch();
+
   return (
     <div className="flex flex-col items-center">
-      <img src={notConnectIcon} alt="not connect" className="mb-10 w-[215px]"/>
+      <img src={notConnectIcon} alt="not connect" className="mb-10 w-[215px]" />
       <p className="text-[#717374] text-sm mb-3">{text}</p>
-      <button className="py-1 px-4 bg-[#006EE9] text-white rounded-md">Connect Wallet</button>
+      <button
+        onClick={() => {
+          dispath(setConnectWalletModal(true));
+        }}
+        className="py-1 px-4 bg-[#006EE9] text-white rounded-md"
+      >
+        Connect Wallet
+      </button>
     </div>
   );
 }
