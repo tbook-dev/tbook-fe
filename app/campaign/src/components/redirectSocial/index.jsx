4c16130637c438e48baf1389d4b3cd24eef1f1bb
lake <posuihushui@icloud.com>
2023-10-14 17:40:54 +0800

    feat: add tw

---
+++ app/campaign/src/components/redirectSocial/index.jsx
@@ -0,0 +1,56 @@
+import Lottie from "lottie-react";
+import redirectJSON from "@/images/social/redirect.json";
+import sucessPNG from "@/images/social/sucess.png";
+import failedPNG from "@/images/social/failed.png";
+import occupiedPNG from "@/images/social/occupied.png";
+
+const Result = ({ title, desc }) => {
+  return (
+    <div className="px-7 pt-1 text-center">
+      <h2 className="text-[#131517] text-base font-medium mb-3">{title}</h2>
+      <p className="text-sm text-[#717374]">{desc}</p>
+    </div>
+  );
+};
+
+// loading, sucess, failed, occupied
+export default function RedirectSocial({
+  status = "loading",
+  desc = "",
+}) {
+  return (
+    <div>
+      {status === "loading" && (
+        <div>
+          <Lottie animationData={redirectJSON} loop={true} />
+          <Result
+            title="Loading..."
+            desc="We are currently verifying the account connection results. Your patience is greatly appreciated."
+          />
+        </div>
+      )}
+
+      {status === "sucess" && (
+        <div>
+          <img src={sucessPNG} className="w-full" />
+          <Result title="Account authorized successfully!" />
+        </div>
+      )}
+      {status === "failed" && (
+        <div>
+          <img src={failedPNG} className="w-full" />
+          <Result
+            title="Account authorization failed"
+            desc="We regret that the account authorization has failed. Please try again later."
+          />
+        </div>
+      )}
+      {status === "occupied" && (
+        <div>
+          <img src={occupiedPNG} className="w-full" />
+          <Result title="Account occupied" desc={desc || 'Account occupied!'} />
+        </div>
+      )}
+    </div>
+  );
+}
--- app/campaign/src/pages/twitter/callback.jsx
+++ app/campaign/src/pages/twitter/callback.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { host } from "@/api/incentive";
+import RedirectSocial from "@/components/redirectSocial";
 
 export default function () {
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState(null);
+  const [status, setStatus] = useState("loading");
 
   useEffect(() => {
     const url = new URL(window.location.href);
@@ -24,19 +26,35 @@ export default function () {
       .then((r) => r.json())
       .then((d) => {
         console.log(d);
-        if (d.code != 200) {
-          setErrorMessage(d.message);
-          return;
-        }
-        const redirect = localStorage.getItem("redirect_url");
-        if (redirect != null) {
-          localStorage.removeItem("redirect_url");
-          location.href = redirect;
+        if (d.code === 4004) {
+          setStatus("occupied");
+          setErrorMessage("");
+        } else if (d.code === 500) {
+          setStatus("failed");
         } else {
-          navigate("/");
+          setStatus("sucess");
+          const redirect = localStorage.getItem("redirect_url");
+          if (redirect != null) {
+            localStorage.removeItem("redirect_url");
+            location.href = redirect;
+          } else {
+            navigate("/");
+          }
         }
+        // if (d.code != 200) {
+        //   setErrorMessage(d.message);
+        //   return;
+        // }
+        // const redirect = localStorage.getItem("redirect_url");
+        // if (redirect != null) {
+        //   localStorage.removeItem("redirect_url");
+        //   location.href = redirect;
+        // } else {
+        //   navigate("/");
+        // }
       })
       .catch((e) => {
+        setStatus("failed");
         const redirect = localStorage.getItem("redirect_url");
         localStorage.removeItem("redirect_url");
         location.href = redirect;
@@ -44,9 +62,12 @@ export default function () {
   }, []);
 
   return (
-    <>
-      <div>Redirecting...</div>
-      {errorMessage && <div>{errorMessage}</div>}
-    </>
+    // <>
+    //   <div>Redirecting...</div>
+    //   {errorMessage && <div>{errorMessage}</div>}
+    // </>
+    <div>
+      <RedirectSocial status={status} desc={errorMessage} />
+    </div>
   );
 }
