066edcfbcb3a7a8742d5e40cf04913b564f71bc7
lake <posuihushui@icloud.com>
2023-10-19 20:40:14 +0800

    lazyload

--- app/campaign/src/components/lazyImage/index.jsx
+++ app/campaign/src/components/lazyImage/index.jsx
@@ -1,13 +1,5 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import banner from '@/images/banner.png'
-const placeholderImage = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0'+
-  'JveD0iMCAwIDE0MCAxNDAiIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj4KICA8cGF0aCBkPSJNMTE4Lj'+
-  'csMTQwIGMwLjYsMC4zLDAuOSwxLjIsMC4zLDEuOGMwLjcsMC40LDEuMywxLjUsMS44LDEuN2MxLjksM'+
-  'CwyLjEsMC45LDIuMSwxLjdzLTEuOC0wLjItMi41LTEuMmMtMC42LTAuMi0xLjItMC40LTEuMi0wLj'+
-  'RzLTEuOCwwLjItMi41LDEuMmMwLjYsMC4yLDEuMi0wLjQsMS4yLTAuNHMxLjgsMC4yLDIuNSwxLj'+
-  'IgYzAuNiwwLjIsMS4yLDAuNCwxLjIsMC40UzEwOS42LDE0MCwxMTguNywxNDAiLz4KPC9zdmc+Cg==';
-
 
 export default function ({ src, alt, className }) {
   return (
@@ -15,7 +7,17 @@ export default function ({ src, alt, className }) {
       src={src}
       alt={alt}
       effect="blur"
-      placeholderSrc={banner}
+      placeholder={
+        <svg
+          width="359"
+          height="172"
+          viewBox="0 0 359 172"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="359" height="172" fill="#F0F0F0" />
+        </svg>
+      }
       className={className}
       width={"100%"}
       height="100%"
