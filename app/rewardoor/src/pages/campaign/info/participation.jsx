40902dd7ddee05b9d610f3f5b6710f99990b822e
lake <posuihushui@icloud.com>
2023-10-20 11:59:43 +0800

    feat: point字段逻辑更改

--- app/rewardoor/src/pages/campaign/info/participation.jsx
+++ app/rewardoor/src/pages/campaign/info/participation.jsx
@@ -237,8 +237,8 @@ export default function Participation() {
                           align="center"
                           className="pb-4 text-base"
                         >
-                          {v.pointList?.length > 0
-                            ? `+${formatDollar(v.number)}`
+                          {v.points?.length > 0
+                            ? `+${formatDollar(v.pointNum)}`
                             : "×"}
                         </td>
                       )}
