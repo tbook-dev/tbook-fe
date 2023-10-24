5bf0a5aa9a181be26bccab828a9262a9b7d08902
lake <posuihushui@icloud.com>
2023-10-24 17:57:53 +0800

    feat: upload img preview

--- app/rewardoor/src/pages/campaign/modules/BasicInfo.jsx
+++ app/rewardoor/src/pages/campaign/modules/BasicInfo.jsx
@@ -1,76 +1,80 @@
-import { Form, Input, Upload, DatePicker } from 'antd'
-import uploadFile, { fileValidator } from '@/utils/upload'
-import uploadIcon from '@/images/icon/upload.svg'
+import { Form, Input, Upload, DatePicker } from "antd";
+import uploadFile, { fileValidator } from "@/utils/upload";
+import uploadIcon from "@/images/icon/upload.svg";
 
-const { RangePicker } = DatePicker
-export default function BasicInfo ({ form: setUpForm }) {
+const { RangePicker } = DatePicker;
+export default function BasicInfo({ form: setUpForm }) {
   const hanleUpload = ({ onSuccess, onError, file }) => {
-    uploadFile(file).then(onSuccess).catch(onError)
-  }
-  const normFile = e => {
-    console.log('Upload event:', e)
+    uploadFile(file).then(onSuccess).catch(onError);
+  };
+  const picUrl = Form.useWatch("picUrl", setUpForm);
+  
+  const normFile = (e) => {
+    console.log("Upload event:", e);
     if (Array.isArray(e)) {
-      return e
+      return e;
     }
-    return e?.fileList
-  }
+    return e?.fileList;
+  };
   return (
-    <Form
-      className='w-[520px]'
-      form={setUpForm}
-      layout='vertical'
-    >
+    <Form className="w-[520px]" form={setUpForm} layout="vertical">
       <Form.Item
-        label='Title'
-        name='title'
-        rules={[{ required: true, message: 'Title is required' }]}
+        label="Title"
+        name="title"
+        rules={[{ required: true, message: "Title is required" }]}
       >
-        <Input placeholder='Enter a campaign title' />
+        <Input placeholder="Enter a campaign title" />
       </Form.Item>
 
       <Form.Item
-        valuePropName='fileList'
+        valuePropName="fileList"
         getValueFromEvent={normFile}
-        label='Poster'
-        name='picUrl'
+        label="Poster"
+        name="picUrl"
         rules={[
           {
             required: true,
-            message: 'Poster is required'
+            message: "Poster is required",
           },
           {
-            validator: fileValidator
-          }
+            validator: fileValidator,
+          },
         ]}
       >
         <Upload.Dragger
           customRequest={hanleUpload}
           multiple={false}
-          accept='image/*'
+          accept="image/*"
           maxCount={1}
         >
-          <p className='ant-upload-drag-icon flex justify-center'>
-            <img src={uploadIcon} />
-          </p>
-          <p className='ant-upload-text'>Upload an image</p>
-          <p className='ant-upload-hint'>296*312 or higher</p>
-          <p className='ant-upload-hint'>recommended Max 20MB.</p>
+          {picUrl?.[0]?.response ? (
+            <img src={picUrl?.[0]?.response} className="w-full h-[180px] object-contain object-center" />
+          ) : (
+            <>
+              <p className="ant-upload-drag-icon flex justify-center">
+                <img src={uploadIcon} />
+              </p>
+              <p className="ant-upload-text">Upload an image</p>
+              <p className="ant-upload-hint">359*172 or higher</p>
+              <p className="ant-upload-hint">recommended Max 20MB.</p>
+            </>
+          )}
         </Upload.Dragger>
       </Form.Item>
       <Form.Item
-        label='Description'
-        name='description'
-        rules={[{ required: true, message: 'Description is required' }]}
+        label="Description"
+        name="description"
+        rules={[{ required: true, message: "Description is required" }]}
       >
-        <Input.TextArea autoSize placeholder='Enter' />
+        <Input.TextArea autoSize placeholder="Enter" />
       </Form.Item>
       <Form.Item
-        label='Schedule'
-        name='schedule'
-        rules={[{ required: true, message: 'Schedule is required' }]}
+        label="Schedule"
+        name="schedule"
+        rules={[{ required: true, message: "Schedule is required" }]}
       >
-        <RangePicker showTime className='w-full' />
+        <RangePicker showTime className="w-full" />
       </Form.Item>
     </Form>
-  )
+  );
 }
