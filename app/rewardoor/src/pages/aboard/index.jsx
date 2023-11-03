ef2d18625f071d83429a76deec4abfd810a4b326
lake <posuihushui@icloud.com>
2023-11-03 19:46:12 +0800

    feat: 左边滑💨

--- app/rewardoor/src/pages/aboard/index.jsx
+++ app/rewardoor/src/pages/aboard/index.jsx
@@ -1,6 +1,4 @@
 import Logo from '@/components/logo'
-import bannerUrl from '@/images/aboard-banner.png'
-import bannerBg from '@/images/aboard-bg.png'
 import metaMask from '@/images/icon/metamask.svg'
 import walletconnect from '@/images/icon/walletconnect.svg'
 import useSignIn from '@/hooks/useSignIn'
@@ -10,9 +8,7 @@ import { useAccount } from 'wagmi'
 import { useEffect } from 'react'
 import { host } from '@/api/incentive'
 import { useState } from 'react'
-
-const titleList = ['Incentivize core', 'communities', 'and builders']
-const p = 'grant easily and optimize continuously '
+import Slide from './slide'
 
 const h1 = 'Get Started on TBOOK'
 const h1Text =
@@ -57,24 +53,8 @@ export default function Aboard () {
 
   return (
     <div className='flex h-screen'>
-      <div className='select-none	w-1/2 relative bg-b-1 rounded-r-4xl flex items-center justify-center'>
-        <Logo />
-        <div className='w-[592px] relative pt-[220px]'>
-          <img src={bannerUrl} className='absolute top-0 left-0 w-full' />
-          <img src={bannerBg} className='absolute top-0 left-0 w-full' />
-
-          <div className='flex flex-col relative z-10'>
-            {titleList.map((item, index) => (
-              <h3
-                key={index}
-                className='text-6.5xl font-extrabold text-colorful1'
-              >
-                {item}
-              </h3>
-            ))}
-            <p className='mt-3 text-3xl font-extrabold'>{p}</p>
-          </div>
-        </div>
+      <div className='select-none	w-1/2 overflow-hidden min-h-full relative p-12 flex flex-col justify-center'>
+        <Slide /> 
       </div>
       <div className='w-1/2  flex items-center justify-center'>
         <div className='w-[560px]'>
