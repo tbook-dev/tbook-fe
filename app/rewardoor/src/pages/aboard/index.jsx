ca0bfbce2a8afd6a50752b7b0dbbf90c910069b3
lake <posuihushui@icloud.com>
2023-11-03 19:54:35 +0800

    feat: update logo

--- app/rewardoor/src/pages/aboard/index.jsx
+++ app/rewardoor/src/pages/aboard/index.jsx
@@ -1,88 +1,93 @@
-import Logo from '@/components/logo'
-import metaMask from '@/images/icon/metamask.svg'
-import walletconnect from '@/images/icon/walletconnect.svg'
-import useSignIn from '@/hooks/useSignIn'
-import Button from '@/components/button'
-import { useNavigate } from 'react-router-dom'
-import { useAccount } from 'wagmi'
-import { useEffect } from 'react'
-import { host } from '@/api/incentive'
-import { useState } from 'react'
-import Slide from './slide'
+import Logo from "@/components/logo";
+import metaMask from "@/images/icon/metamask.svg";
+import walletconnect from "@/images/icon/walletconnect.svg";
+import useSignIn from "@/hooks/useSignIn";
+import Button from "@/components/button";
+import { useNavigate } from "react-router-dom";
+import { useAccount } from "wagmi";
+import { useEffect } from "react";
+import { host } from "@/api/incentive";
+import { useState } from "react";
+import Slide from "./slide";
 
-const h1 = 'Get Started on TBOOK'
-const h1Text =
-  'TBOOK is your one-stop for branding, marketing, growth and analysis for Web3. Connect your wallet now and start setting up Campaigns.'
+const h1 = "Get started on TBOOK";
+const h1Text = "Connect your wallet now and start setting up Campaigns.";
 
-export default function Aboard () {
-  const { isConnected } = useAccount()
-  const navigate = useNavigate()
-  const { loading, handleSignIn, signMessage } = useSignIn()
-  const [ autoFetch, setAutoFetch ] = useState(true)
+export default function Aboard() {
+  const { isConnected } = useAccount();
+  const navigate = useNavigate();
+  const { loading, handleSignIn, signMessage } = useSignIn();
+  const [autoFetch, setAutoFetch] = useState(true);
 
   const clickSignIn = async (useWc) => {
-    setAutoFetch(false); 
-    await handleSignIn(useWc) 
-    await signMessage()
-  }
+    setAutoFetch(false);
+    await handleSignIn(useWc);
+    await signMessage();
+  };
 
   useEffect(() => {
     if (isConnected && autoFetch) {
       fetch(`${host}/info`, {
-        method: 'GET',
-        credentials: 'include'
-      }).then(r => {
-        console.log({autoFetch})
-        if (!autoFetch) return
+        method: "GET",
+        credentials: "include",
+      }).then((r) => {
+        console.log({ autoFetch });
+        if (!autoFetch) return;
         let p;
         if (r.status === 401) {
-          p = handleSignIn()
-        } else { 
-          p = r.json()
+          p = handleSignIn();
+        } else {
+          p = r.json();
         }
-        p.then(res => {
+        p.then((res) => {
           if (res?.projects?.length === 0) {
-            navigate('/new-project')
+            navigate("/new-project");
           } else {
-            navigate('/')
+            navigate("/");
           }
-        })
-    })
+        });
+      });
     }
-  }, [isConnected, autoFetch])
+  }, [isConnected, autoFetch]);
 
   return (
-    <div className='flex h-screen'>
-      <div className='select-none	w-1/2 overflow-hidden min-h-full relative p-12 flex flex-col justify-center'>
-        <Slide /> 
+    <div className="flex h-screen">
+      <div className="select-none	w-1/2 overflow-hidden min-h-full relative p-12 flex flex-col justify-center">
+        <Slide />
       </div>
-      <div className='w-1/2  flex items-center justify-center'>
-        <div className='w-[560px]'>
-          <h1 className='text-5xl font-extrabold'>{h1}</h1>
-          <p className='text-base font-medium'>{h1Text}</p>
-          <div className='mt-10 space-y-6'>
+      <div className="w-1/2  flex items-center justify-center">
+        <div className="w-[560px] space-y-12">
+          <div>
+            <Logo className="w-8 h-8 inline-block"/>
+          </div>
+          <div>
+            <h1 className="text-[40px] leading-[40px] font-bold mb-6">{h1}</h1>
+            <p className="text-base text-[#F0F0F0]">{h1Text}</p>
+          </div>
+
+          <div className="mt-10 space-y-6">
             {isConnected ? (
-              <div className='flex items-center gap-x-4'>
+              <div className="flex items-center gap-x-4">
                 <w3m-button />
                 Loading……
               </div>
             ) : (
               <>
                 <Button
-                  className='w-full text-base font-bold text-white'
-                  onClick={() => clickSignIn(false) }
+                  className="w-full text-base font-bold text-white"
+                  onClick={() => clickSignIn(false)}
                   loading={loading}
                 >
-                  <img src={metaMask} className='mr-3 w-5 h-5 object-contain' />
+                  <img src={metaMask} className="mr-3 w-5 h-5 object-contain" />
                   MetaMask
                 </Button>
                 <Button
-                  className='w-full text-base font-bold text-white'
-                  onClick={() => clickSignIn(true) }
+                  className="w-full text-base font-bold text-white"
+                  onClick={() => clickSignIn(true)}
                 >
                   <img
                     src={walletconnect}
-                    className='mr-3 w-5 h-5 object-contain'
+                    className="mr-3 w-5 h-5 object-contain"
                   />
                   WalletConnect
                 </Button>
@@ -92,5 +97,5 @@ export default function Aboard () {
         </div>
       </div>
     </div>
-  )
+  );
 }
