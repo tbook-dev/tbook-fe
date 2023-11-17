da848967b14f2b5036901b093d104dec03b76172
lake <lake@krquant.com>
2023-11-17 22:14:25 +0800

    feat: update footer

--- app/home/src/layout/Header.jsx
+++ app/home/src/layout/Header.jsx
@@ -1,62 +1,57 @@
-import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { Drawer } from "antd";
-import { useResponsive } from "ahooks";
-import clsx from "clsx";
-import logo from "@tbook/share/images/icon/logo.svg";
-import menuIcon from "@tbook/share/images/icon/menu.svg";
-import DarkProvider from "@/theme/DarkProvider";
-import MobleMenu from "./MobleMenu";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useResponsive } from 'ahooks'
+import logo from '@tbook/share/images/icon/logo.svg'
+import MobleMenu from './MobleMenu'
 
-const links = [
+export const links = [
   {
-    text: "Home",
-    anchor: "home",
+    text: 'Home',
+    anchor: 'home'
   },
   {
-    text: "Solution",
-    anchor: "solution",
+    text: 'Solution',
+    anchor: 'solution'
   },
   {
-    text: "About",
-    anchor: "about",
+    text: 'About',
+    anchor: 'about'
   },
   {
-    text: "Blog",
-    anchor: "blog",
+    text: 'Blog',
+    anchor: 'blog'
   },
   {
-    text: "Contact",
-    anchor: "contact",
-  },
-];
+    text: 'Contact',
+    anchor: 'contact'
+  }
+]
 
-function Header() {
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const { pc } = useResponsive();
+function Header () {
+  const { pc } = useResponsive()
 
   return (
-    <header className="sticky top-0 z-30">
-      <div className="bx px-4 lg:px-0">
-        <div className="flex items-center justify-between h-14 lg:h-16">
-          <div className="flex items-center">
-            <Link to="/" className="mr-1 lg:mr-16">
-              <img src={logo} className="h-8 lg:h-7" />
+    <header className='sticky top-0 z-30'>
+      <div className='bx px-4 lg:px-0'>
+        <div className='flex items-center justify-between h-14 lg:h-16'>
+          <div className='flex items-center'>
+            <Link to='/' className='mr-1 lg:mr-16'>
+              <img src={logo} className='h-8 lg:h-7' />
             </Link>
           </div>
 
           {pc ? (
-            <div className="flex items-center space-x-12">
-              {links.map((link) => {
+            <div className='flex items-center space-x-12'>
+              {links.map(link => {
                 return (
                   <a
                     key={link.anchor}
                     href={`#${link.anchor}`}
-                    className="text-[#131517] font-medium hover:opacity-70"
+                    className='text-[#131517] font-medium hover:opacity-70'
                   >
                     {link.text}
                   </a>
-                );
+                )
               })}
             </div>
           ) : (
@@ -65,7 +60,7 @@ function Header() {
         </div>
       </div>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
