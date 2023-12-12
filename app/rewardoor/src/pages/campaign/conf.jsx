558eeede6dbbb625d484ff7f4e57bb905af43f71
lake <lake@krquant.com>
2023-12-12 15:41:36 +0800

    feat: use tw statisc

---
+++ app/rewardoor/src/pages/campaign/conf.jsx
@@ -0,0 +1,17 @@
+import { shortAddress } from '@tbook/utils/lib/conf'
+import xIcon from '@/images/icon/x.svg'
+export const defaultCredentialReward = {
+  credential: [],
+  reward: []
+}
+
+export const getParicipant = v => {
+  const { isTwitterLogin, wallet } = v
+  return isTwitterLogin ? (
+    <span className='flex items-center gap-x-0.5'>
+      {wallet} <img src={xIcon} alt='x icon' />
+    </span>
+  ) : (
+    shortAddress(wallet)
+  )
+}
--- app/rewardoor/src/pages/campaign/info/winnerListModal.jsx
+++ app/rewardoor/src/pages/campaign/info/winnerListModal.jsx
@@ -1,52 +1,53 @@
-import { Modal } from "antd";
-import { conf as tbookConf } from "@tbook/utils";
+import { Modal } from 'antd'
+import { conf as tbookConf } from '@tbook/utils'
+import { getParicipant } from '../conf'
 
-const { formatDollar, shortAddress } = tbookConf;
+const { formatDollar } = tbookConf
 
-export default function WhiteList({ data, closeModal, open, modalType, per }) {
-  // console.log({ data });
-  const total = data?.filter((v) => v.claimType === 4)?.length * per;
+export default function WhiteList ({ data, closeModal, open, modalType, per }) {
+  // console.log({ data })
+  const total = data?.filter(v => v.claimType === 4)?.length * per
   return (
     <Modal open={open} width={480} footer={null} centered onCancel={closeModal}>
-      <div className="-mx-6 pt-6">
-        <h2 className="px-5 text-lg text-t-3 font-medium mb-4 flex items-center justify-between">
+      <div className='-mx-6 pt-6'>
+        <h2 className='px-5 text-lg text-t-3 font-medium mb-4 flex items-center justify-between'>
           <span>{formatDollar(data?.length)} Winners</span>
-          <span className="text-c-9 text-sm">
-            <span className="text-t-3 text-xl">{formatDollar(total)}</span>
-            {` ${modalType}${total > 1 ? "s" : ""} `}
+          <span className='text-c-9 text-sm'>
+            <span className='text-t-3 text-xl'>{formatDollar(total)}</span>
+            {` ${modalType}${total > 1 ? 's' : ''} `}
             claimed
           </span>
         </h2>
-        <div className="px-5 py-4 space-y-3 max-h-[428px] overflow-y-auto border-t border-[rgb(50,50,50)] ">
+        <div className='px-5 py-4 space-y-3 max-h-[428px] overflow-y-auto border-t border-[rgb(50,50,50)] '>
           {data?.length > 0
-            ? data?.map((v) => {
+            ? data?.map(v => {
                 return (
                   <div
-                    className="h-8 flex items-center justify-between"
+                    className='h-8 flex items-center justify-between'
                     key={v.userId}
                   >
-                    <div className="flex items-center gap-x-3 text-sm text-t-3">
-                      <div className="w-8 h-8 overflow-hidden rounded-full">
+                    <div className='flex items-center gap-x-3 text-sm text-t-3'>
+                      <div className='w-8 h-8 overflow-hidden rounded-full'>
                         <img
                           src={v.user.avatar}
-                          className="w-full h-full object-contain object-center"
+                          className='w-full h-full object-contain object-center'
                         />
                       </div>
-                      {shortAddress(v.user.wallet)}
+                      {getParicipant(v.user)}
                     </div>
-                    <div className="text-sm">
+                    <div className='text-sm'>
                       {v.claimType === 4 ? (
-                        <span className="text-c-9">Claimed</span>
+                        <span className='text-c-9'>Claimed</span>
                       ) : (
-                        <span className="text-c-6">Waiting to claim</span>
+                        <span className='text-c-6'>Waiting to claim</span>
                       )}
                     </div>
                   </div>
-                );
+                )
               })
-            : "no data!"}
+            : 'no data!'}
         </div>
       </div>
     </Modal>
-  );
+  )
 }
