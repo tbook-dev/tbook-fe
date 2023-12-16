a07b1f88afaa5b0ff732f74f98f8692320ef2914
lake <posuihushui@icloud.com>
2023-12-17 07:40:28 +0800

    remove blog

--- app/home/src/pages/home/index.jsx
+++ app/home/src/pages/home/index.jsx
@@ -64,7 +64,7 @@ export default function Home () {
       <Banner />
       <Solution />
       <About />
-      <Blog />
+      {/* <Blog /> */}
       <Calendly />
     </div>
   )
--- app/home/src/partials/home/banner/index.jsx
+++ app/home/src/partials/home/banner/index.jsx
@@ -12,14 +12,14 @@ const pageConf = {
     prefix: 'optimize',
     aspects: [
       'core',
-      'token economy',
+      'economy',
       'governance',
       'protocol design',
       'incentive program'
     ],
     surfix: 'of future financial system'
   },
-  desc: 'We help industry to navigate critical decisions, elevate token economy as a whole'
+  desc: 'We help industry to navigate critical decisions, elevate economy as a whole'
 }
 
 export default function () {
