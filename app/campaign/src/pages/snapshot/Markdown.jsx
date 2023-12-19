cdc2811d0a0c300ddccb6542624bf4ae109ff76f
lake <lake@krquant.com>
2023-12-19 21:41:13 +0800

    fix:snapshot

--- app/campaign/src/pages/snapshot/Markdown.jsx
+++ app/campaign/src/pages/snapshot/Markdown.jsx
@@ -1,37 +1,73 @@
-import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
-import "./github-markdown-light.css";
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+import './github-markdown-light.css'
+import { useEffect, useState, useRef } from 'react'
+import clsx from 'clsx'
 
+export default function Markdown ({ children }) {
+  const [showMore, setShowMore] = useState(false)
+  const [hasMore, setHasMore] = useState(false)
+  const textRef = useRef(null)
+  useEffect(() => {
+    calcFn()
+  }, [children])
 
+  const calcFn = () => {
+    if (textRef.current) {
+      const { offsetHeight, scrollHeight } = textRef.current
+      if (scrollHeight > offsetHeight) {
+        setHasMore(true)
+      }
+    }
+  }
 
-export default function Markdown({ children }) {
+  const handleOnload = () => {
+    calcFn()
+  }
+
   return (
-    <ReactMarkdown
-      className="markdown-body"
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || "");
-          return !inline && match ? (
-            <SyntaxHighlighter
-              children={String(children).replace(/\n$/, "")}
-              style={coldarkDark}
-              language={match[1]}
-              PreTag="div"
-              showLineNumbers={false}
-              {...props}
-            />
-          ) : (
-            <code className={className} {...props} children={children} />
-          );
-        },
-      }}
-    >
-      {children}
-    </ReactMarkdown>
-  );
+    <div>
+      <div
+        className={clsx(showMore ? '' : 'h-[180px] overflow-hidden')}
+        ref={textRef}
+        onLoad={handleOnload}
+      >
+        <ReactMarkdown
+          className='markdown-body'
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
+          components={{
+            code ({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '')
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  style={coldarkDark}
+                  language={match[1]}
+                  PreTag='div'
+                  showLineNumbers={false}
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props} children={children} />
+              )
+            }
+          }}
+        >
+          {children}
+        </ReactMarkdown>
+      </div>
+      {hasMore && (
+        <button
+          onClick={() => setShowMore(v => !v)}
+          className='text-[#904BF6] mt-1'
+        >
+          {showMore ? 'Show Less' : 'Show More'}
+        </button>
+      )}
+    </div>
+  )
 }
