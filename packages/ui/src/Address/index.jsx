dd3491d607eef106f05abb93890b95277e066919
lake <lake@krquant.com>
2023-12-26 18:52:31 +0800

    feat: update copy icon color

--- packages/ui/src/Address/index.jsx
+++ packages/ui/src/Address/index.jsx
@@ -1,11 +1,21 @@
 import { Tooltip, Typography } from 'antd'
+import { CopyOutlined, CheckOutlined } from '@ant-design/icons'
 import { shortAddress } from '@tbook/utils/lib/conf'
 
 const { Paragraph } = Typography
 
 export default function Address ({ address, placement = 'top' }) {
   return (
-    <Paragraph copyable={{ text: address }} style={{ margin: 0 }}>
+    <Paragraph
+      copyable={{
+        text: address,
+        icon: [
+          <CopyOutlined style={{ color: '#717374' }} />,
+          <CheckOutlined style={{ color: '#717374' }} />
+        ]
+      }}
+      style={{ margin: 0 }}
+    >
       <Tooltip title={address} placement={placement}>
         {shortAddress(address)}
       </Tooltip>
