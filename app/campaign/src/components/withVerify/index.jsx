d2e583086dbb05e5a2ee18d974e1e36736f62c31
lake <lake@krquant.com>
2023-12-27 19:30:24 +0800

    feat: set time out after verify action

--- app/campaign/src/components/withVerify/index.jsx
+++ app/campaign/src/components/withVerify/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Spin, Modal } from 'antd'
 import { useResponsive } from 'ahooks'
 import clsx from 'clsx'
@@ -38,7 +38,8 @@ export default function WithVerify ({
   const [status, setStatus] = useState(verifyStatusEnum.NotStarted)
   const { userLogined, wallectConnected } = useUserInfo()
   const dispath = useDispatch()
-
+  const [count, setCount] = useState(0)
+  const clearInterIdRef = useRef()
   const sysLogined = sysConnectedMap[credentialType]
   const social = getSocialByName(credentialType)
   const isSocial = !!social
@@ -52,16 +53,31 @@ export default function WithVerify ({
     }
     await delay(1000)
     handleCancel()
+    setCount(30)
   }
   const handleCancel = useCallback(() => {
     setOpen(false)
   }, [])
+
+  useEffect(() => {
+    clearInterIdRef.current = setInterval(() => {
+      if (count > 0) {
+        setCount(v => v - 1)
+      } else {
+        clearInterval(clearInterIdRef.current)
+      }
+    }, 1000)
+    return () => {
+      clearInterval(clearInterIdRef.current)
+    }
+  }, [count])
+
   // console.log({social})
 
   return (
     <>
       <button
-        disabled={status === verifyStatusEnum.Pending}
+        disabled={status === verifyStatusEnum.Pending || count > 0}
         className={clsx(
           'text-base whitespace-nowrap px-1.5 py-1 rounded',
           'flex items-center gap-x-1',
@@ -70,15 +86,17 @@ export default function WithVerify ({
               verifyStatusEnum.Sucess,
               verifyStatusEnum.Pending
             ].includes(status),
-            'text-white bg-[#904BF6]': status === verifyStatusEnum.NotStarted
+            'text-white bg-[#904BF6]': status === verifyStatusEnum.NotStarted,
+            'bg-[#2B174A] text-[#55456E] w-[78px] justify-center cursor-not-allowed':
+              count > 0
           }
         )}
         onClick={evt => {
           if (!userLogined) {
             dispath(setLoginModal(true))
-          } else if(!wallectConnected){
+          } else if (!wallectConnected) {
             dispath(setConnectWalletModal(true))
-          }else {
+          } else {
             if (isSocial) {
               setOpen(true)
             } else {
@@ -87,10 +105,16 @@ export default function WithVerify ({
           }
         }}
       >
-        <VerifyStatus status={status} />
-        {status === verifyStatusEnum.Sucess && 'Verified'}
-        {status === verifyStatusEnum.Pending && 'Verify...'}
-        {status === verifyStatusEnum.NotStarted && 'Verify'}
+        {count > 0 ? (
+          `${count}s`
+        ) : (
+          <>
+            <VerifyStatus status={status} />
+            {status === verifyStatusEnum.Sucess && 'Verified'}
+            {status === verifyStatusEnum.Pending && 'Verify...'}
+            {status === verifyStatusEnum.NotStarted && 'Verify'}
+          </>
+        )}
       </button>
 
       {isSocial && (
