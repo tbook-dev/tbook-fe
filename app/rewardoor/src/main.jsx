334e228f7bfb70a7c171f091ab7f59cf13cfc63b
lake <posuihushui@icloud.com>
2024-01-08 21:24:18 +0800

    no retry golal setting

--- app/rewardoor/src/main.jsx
+++ app/rewardoor/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Theme from './theme/ThemeProvider'
 import { QueryClient, QueryClientProvider } from 'react-query'
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({ retry: false })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
