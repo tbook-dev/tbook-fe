5402a7ab36e46eda0cc697cfdcca081424090c42
lake <posuihushui@icloud.com>
2024-02-01 17:22:46 +0800

    f: use local files not tiny.clod

--- app/rewardoor/src/components/mce/FormItem.jsx
+++ app/rewardoor/src/components/mce/FormItem.jsx
@@ -1,30 +1,26 @@
-import { Editor } from "@tinymce/tinymce-react";
+// import { Editor } from "@tinymce/tinymce-react";
+import BundledEditor from './BundledEditor'
 
 export default function FormItem({ onChange, value }) {
   return (
-    <Editor
-      apiKey="1sbhkejkzk9owvvikl43hj814a6ri0qed0c8pr53waolkm8q"
+    <BundledEditor
       init={{
         selector: 'textarea',
-        height: 500,
         menubar: false,
         // theme: "oxide-dark",
-        skin: "oxide-dark",
-        content_css: "dark",
-        plugins:
-          "tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
-        toolbar:
-          "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-        tinycomments_mode: "embedded",
-        tinycomments_author: "Author name",
-        mergetags_list: [
-          { value: "First.Name", title: "First Name" },
-          { value: "Email", title: "Email" },
+        // inline: true,
+        // plugins:
+        //   "autoresize mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+        // toolbar:
+        //   "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+        plugins: [
+          'advlist', 'anchor', 'autolink', 'image', 'link', 'lists',
+          'searchreplace', 'table', 'wordcount'
         ],
-        // ai_request: (request, respondWith) =>
-        //   respondWith.string(() =>
-        //     Promise.reject("See docs to implement AI Assistant")
-        //   ),
+        toolbar: 'undo redo | blocks | ' +
+          'bold italic forecolor | alignleft aligncenter ' +
+          'alignright alignjustify | bullist numlist outdent indent | ' +
+          'removeformat',
       }}
       value={value}
       onEditorChange={(v) => {
