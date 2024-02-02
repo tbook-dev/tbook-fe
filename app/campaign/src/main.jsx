cc52460c6bda6fb3da6c2e355f4ac8c4c23b965e
gamma916 <garfeildma@yandex.com>
2024-02-02 18:31:19 +0800

    use enoki sdk for zk login

--- app/campaign/src/main.jsx
+++ app/campaign/src/main.jsx
@@ -8,13 +8,18 @@ import { Provider } from 'react-redux'
 import { QueryClientProvider } from 'react-query'
 import queryClient from './query-client'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { EnokiFlowProvider } from '@mysten/enoki/react';
 
+const enoki_key = import.meta.env.VITE_ENOKI_API_KEY
+const baseUrl = import.meta.env.VITE_API_HOST
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <Theme>
+        <EnokiFlowProvider apiKey={enoki_key} apiUrl={`${baseUrl}/zkproxy`}>
         <App />
+        </EnokiFlowProvider>
       </Theme>
     </QueryClientProvider>
   </Provider>
---
+++ app/campaign/src/pages/zklogin/zk_login_with_enoki.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { useEnokiFlow, useZkLogin, useAuthCallback } from '@mysten/enoki/react';
+import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export default function Home() {
+    const flow = useEnokiFlow();
+    const zkLogin = useZkLogin();
+    const [result, setResult] = useState(null);
+    const host = new URL(location.href).host;
+    const callback_url = `https://${host}/zklogin/callback`;
+
+    useAuthCallback();
+
+    return (
+        <div>
+            <div>Address: {zkLogin.address}</div>
+            <div>Provider: {zkLogin.provider}</div>
+            {!zkLogin.address ? (
+                <button
+                    onClick={async () => {
+                        window.location.href = await flow.createAuthorizationURL({
+                            provider: 'google',
+                            clientId: googleClientId,
+                            redirectUrl: callback_url,
+                            extraParams: {scope: ['email']}
+                        });
+                    }}
+                >
+                    Sign in with Google
+                </button>
+            ) : (
+                <button onClick={() => flow.logout()}>Sign Out</button>
+            )}
+
+            {zkLogin.address && (
+                <button
+                    onClick={async () => {
+                        try {
+                            const transactionBlock = new TransactionBlock();
+                            transactionBlock.moveCall({
+                                target:
+                                    '0xfa0e78030bd16672174c2d6cc4cd5d1d1423d03c28a74909b2a148eda8bcca16::clock::access',
+                                arguments: [transactionBlock.object('0x6')],
+                            });
+
+                            const result = await flow.sponsorAndExecuteTransactionBlock({
+                                network: 'testnet',
+                                // @ts-expect-error: Type references not quite doing their thing:
+                                client: new SuiClient({ url: getFullnodeUrl('testnet') }),
+                                // @ts-expect-error: Type references not quite doing their thing:
+                                transactionBlock,
+                            });
+
+                            setResult(result);
+                        } catch (e) {
+                            console.log(e);
+                            setResult({ error: e });
+                        }
+                    }}
+                >
+                    Sign transaction
+                </button>
+            )}
+
+            {result && <div>{JSON.stringify(result)}</div>}
+        </div>
+    )
+}
