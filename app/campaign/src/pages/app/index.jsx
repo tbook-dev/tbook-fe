2b87739ff237e464f4a250ab0990ad86df323cee
lake <posuihushui@icloud.com>
2024-02-21 20:30:10 +0800

    update point icon

--- app/campaign/src/pages/app/index.jsx
+++ app/campaign/src/pages/app/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react'
 import { logUserReport } from '@/api/incentive'
 import { useParams } from 'react-router-dom'
-import pointIcon from '@/images/icon/point.svg'
+import pointIcon from '@/images/icon/point-modal.svg'
 import arrow3Icon from '@/images/icon/arrow3.svg'
 import useUserInfo from '@/hooks/useUserInfoQuery'
 import useCampaignQuery from '@/hooks/useCampaignQuery'
