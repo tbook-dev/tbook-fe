6af8b099d73139532903eebcbef18202a35a565c
lake <posuihushui@icloud.com>
2024-03-13 17:42:14 +0800

    update merge account style

--- app/campaign/src/components/account/MergeAccount.jsx
+++ app/campaign/src/components/account/MergeAccount.jsx
@@ -4,11 +4,17 @@ import { setShowMergeAccountModal } from '@/store/global'
 import { Dialog, Transition } from '@headlessui/react'
 
 const moduleConf = {
-  title: 'Merge the passport accounts',
-  button: ['Cancel', 'Merge'],
-  desc: 'All data from these 2 accounts will be merged together. This action cannot be undone.',
-  getAccountInfo: ({ twitterName ='', address='' }) => {
-    return `Are you sure to merge the passport accounts logged in with Twitter @${twitterName} and the logged in with the wallet address ${address}? `
+  title: 'Confirm to merge ?',
+  button: ['Cancel', 'Confirm'],
+  desc: 'This action cannot be reverted.',
+  getAccountInfo: ({ twitterName = '', address = '' }) => {
+    return (
+      <>
+        All data from the incentive passport associated with X @<span className='text-white font-medium'>{twitterName}</span>
+        and the incentive passport associated with address $<span className='text-white font-medium'>{address}</span> will be
+        merged. 
+      </>
+    )
   }
 }
 export default function MergeAccount () {
@@ -19,6 +25,10 @@ export default function MergeAccount () {
   const hideMergeAccountModal = useCallback(() => {
     dispath(setShowMergeAccountModal(false))
   }, [])
+
+  const handleMergeAccount = () => {
+    console.log('confirm')
+  }
 
   return (
     <Transition.Root show={showMergeAccountModal} as={Fragment}>
@@ -37,11 +47,11 @@ export default function MergeAccount () {
           leaveFrom='opacity-100'
           leaveTo='opacity-0'
         >
-          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+          <div className='fixed inset-0 bg-black bg-opacity-75 backdrop-blur transition-opacity' />
         </Transition.Child>
 
         <div className='fixed inset-0 z-10 overflow-y-auto'>
-          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+          <div className='flex min-h-full justify-center p-4 text-center items-center sm:p-0'>
             <Transition.Child
               as={Fragment}
               enter='ease-out duration-300'
@@ -51,47 +61,63 @@ export default function MergeAccount () {
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-                <div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-linear2 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+                <div className='px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
                   <div className='sm:flex sm:items-start'>
-                    <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
-                      <div
-                        className='h-6 w-6 text-red-600'
-                        aria-hidden='true'
-                      />
+                    <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-[rgb(69,10,10)]/[0.25] sm:mx-0 sm:h-10 sm:w-10'>
+                      <svg
+                        width='40'
+                        height='40'
+                        viewBox='0 0 40 40'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <rect
+                          width='40'
+                          height='40'
+                          rx='20'
+                          fill='#450A0A'
+                          fillOpacity='0.25'
+                        />
+                        <path
+                          d='M20 17V19M20 23H20.01M13.0718 27H26.9282C28.4678 27 29.4301 25.3333 28.6603 24L21.7321 12C20.9623 10.6667 19.0378 10.6667 18.268 12L11.3398 24C10.57 25.3333 11.5322 27 13.0718 27Z'
+                          stroke='#DC2626'
+                          strokeWidth='2'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                        />
+                      </svg>
                     </div>
-                    <div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
+                    <div className='mt-3 sm:ml-4 sm:mt-0'>
                       <Dialog.Title
                         as='h3'
-                        className='text-base font-semibold leading-6 text-gray-900'
+                        className='text-lg font-medium text-white text-center lg:text-left'
                       >
                         {moduleConf.title}
                       </Dialog.Title>
-                      <div className='mt-2'>
-                        <p className='text-sm text-gray-500'>
+                      <div className='mt-2 text-[#C0ABD9]'>
+                        <p className='text-sm'>
                           {moduleConf.getAccountInfo(mergeAccountData)}
                         </p>
-                        <p className='text-sm text-gray-500'>
-                          {moduleConf.desc}
-                        </p>
+                        <p className='text-sm'>{moduleConf.desc}</p>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
+                <div className='px-4 py-3 flex flex-col-reverse gap-y-3 lg:px-6 lg:flex-row lg:justify-end lg:gap-x-3'>
                   <button
                     type='button'
-                    className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
+                    className='inline-flex w-full justify-center rounded-md border border-white px-3 py-2 text-sm font-semibold text-white shadow-sm hover:opacity-70 sm:w-auto'
                     onClick={hideMergeAccountModal}
                   >
-                    Deactivate
+                    {moduleConf.button[0]}
                   </button>
                   <button
                     type='button'
-                    className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
-                    onClick={hideMergeAccountModal}
+                    className='inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-black shadow-sm sm:w-auto hover:opacity-70'
+                    onClick={handleMergeAccount}
                   >
-                    Cancel
+                    {moduleConf.button[1]}
                   </button>
                 </div>
               </Dialog.Panel>
