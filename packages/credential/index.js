e28358cad08701ec758d6893490343305e5e0dbb
lake <posuihushui@icloud.com>
2024-03-16 23:31:06 +0800

    fix jump link

--- packages/credential/index.js
+++ packages/credential/index.js
@@ -1,3 +1,4 @@
 export { default as category } from './category';
 export { default as credential } from './credential';
+export { default as actionMap } from './action';
 export { default as Display } from './display';
