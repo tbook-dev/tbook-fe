727378afc502452e20b7dbe1bbf66a550efcec0c
lake <posuihushui@icloud.com>
2024-03-28 15:32:11 +0800

    base64 decode and encode

--- app/campaign/src/pages/ton-explore/index.jsx
+++ app/campaign/src/pages/ton-explore/index.jsx
@@ -2,9 +2,33 @@ import Page404 from '@/pages/404';
 import { useTelegram } from '@/hooks/useTg';
 import Page from './page';
 import Layout from '@/layout/ton/Layout';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
-export default function TonExplore () {
-  const { isTMA } = useTelegram();
+function safeParse(start_param) {
+  try {
+    const str = atob(start_param);
+    return JSON.parse(str);
+  } catch (error) {
+    return {};
+  }
+}
+export default function TonExplore() {
+  const { isTMA, webApp } = useTelegram();
+  const navigate = useNavigate();
+  console.log({ webApp, initDataUnsafe: webApp?.initDataUnsafe });
+
+  useEffect(() => {
+    if (webApp?.initDataUnsafe.start_param) {
+      const { type, projectUrl, campaignId } = safeParse(
+        webApp?.initDataUnsafe.start_param
+      );
+      if (type === 'campaign' && !redirected) {
+        navigate(`/${projectUrl}/${campaignId}`);
+      }
+    }
+  }, []);
+
   return isTMA ? (
     <Layout>
       <Page />
