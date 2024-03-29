4cda7df87a6e4b6292fc7e49edd5b533930bf104
lake <posuihushui@icloud.com>
2024-03-29 11:19:26 +0800

    tma not has logout func

--- app/campaign/src/layout/common/Avatar.jsx
+++ app/campaign/src/layout/common/Avatar.jsx
@@ -8,6 +8,7 @@ import Address from '@tbook/ui/src/Address';
 import Modal from '@/components/connectWallet/modal';
 import PassportCard from '@/components/passportGen/card';
 import { useTonConnectUI } from '@tonconnect/ui-react';
+import { useTelegram } from '@/hooks/useTg';
 
 export default function Avatar () {
   const [open, setOpen] = useState(false);
@@ -15,6 +16,7 @@ export default function Avatar () {
   const { getZkfnByName, getSocialByName } = useSocial();
   const { isConnected } = useAccount();
   const [tonConnectUI] = useTonConnectUI();
+  const { isTMA } = useTelegram();
   const handleLogout = useCallback(async () => {
     if (tonConnectUI.connected) {
       await tonConnectUI.disconnect();
@@ -89,30 +91,32 @@ export default function Avatar () {
           setOpen(false);
         }}
       >
-        <div className='px-6 py-4 flex-none'>
-          <div
-            className='text-[#C0ABD9] cursor-pointer flex items-center group hover:text-white gap-x-1 text-base'
-            onClick={handleLogout}
-          >
-            <svg
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
+        {!isTMA && (
+          <div className='px-6 py-4 flex-none'>
+            <div
+              className='text-[#C0ABD9] cursor-pointer flex items-center group hover:text-white gap-x-1 text-base'
+              onClick={handleLogout}
             >
-              <path
-                d='M14.1667 10.6667V7.75C14.1667 7.28587 13.9823 6.84075 13.6541 6.51256C13.3259 6.18437 12.8808 6 12.4167 6H7.75C7.28587 6 6.84075 6.18437 6.51256 6.51256C6.18437 6.84075 6 7.28587 6 7.75V18.25C6 18.7141 6.18437 19.1592 6.51256 19.4874C6.84075 19.8156 7.28587 20 7.75 20H12.4167C12.8808 20 13.3259 19.8156 13.6541 19.4874C13.9823 19.1592 14.1667 18.7141 14.1667 18.25V15.3333M16.5 15.3333L18.8333 13M18.8333 13L16.5 10.6667M18.8333 13H8.91667'
-                stroke=''
-                className='group-hover:stroke-white stroke-[#C0ABD9]'
-                strokeWidth='1.5'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              />
-            </svg>
-            Logout
+              <svg
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M14.1667 10.6667V7.75C14.1667 7.28587 13.9823 6.84075 13.6541 6.51256C13.3259 6.18437 12.8808 6 12.4167 6H7.75C7.28587 6 6.84075 6.18437 6.51256 6.51256C6.18437 6.84075 6 7.28587 6 7.75V18.25C6 18.7141 6.18437 19.1592 6.51256 19.4874C6.84075 19.8156 7.28587 20 7.75 20H12.4167C12.8808 20 13.3259 19.8156 13.6541 19.4874C13.9823 19.1592 14.1667 18.7141 14.1667 18.25V15.3333M16.5 15.3333L18.8333 13M18.8333 13L16.5 10.6667M18.8333 13H8.91667'
+                  stroke=''
+                  className='group-hover:stroke-white stroke-[#C0ABD9]'
+                  strokeWidth='1.5'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+              Logout
+            </div>
           </div>
-        </div>
+        )}
 
         <PassportCard
           onClose={() => {
