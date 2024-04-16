a5f778cf75874ae78721871d0513bbcae91b5f91
lake <posuihushui@icloud.com>
2024-04-16 10:49:44 +0800

    feat: add new dc invite ui

--- app/rewardoor/src/components/credential/conf/Community.jsx
+++ app/rewardoor/src/components/credential/conf/Community.jsx
@@ -1,6 +1,8 @@
 import { Form, Input } from 'antd';
 const FormItem = Form.Item;
 
+const dcBotLink =
+  'https://discord.com/oauth2/authorize?client_id=1146414186566537288&permissions=2417034321&scope=bot';
 export default {
   // twitter
   11: {
@@ -104,23 +106,47 @@ export default {
   4: {
     render: ({ name }) => {
       return (
-        <FormItem
-          label='Discord Server URL'
-          name={[name, 'link']}
-          rules={[
-            {
-              required: true,
-              message: 'Please input the Discord Server URL',
-            },
-            {
-              required: false,
-              message: 'Please input a valid url',
-              type: 'url',
-            },
-          ]}
-        >
-          <Input placeholder='https://discord.gg/xxxx!' />
-        </FormItem>
+        <div className='space-y-3'>
+          <div className='text-sm font-medium text-c-9 flex justify-between'>
+            <div>
+              <span className='text-[#FF5151] me-1'>*</span>
+              Invite Tbook Bot as an admin to your server
+            </div>
+            <a
+              className='underline text-[#717374] hover:opacity-70 hover:text-white hover:underline'
+              href='https://app.gitbook.com/o/XmLEuzCUK0IIbhY5X44k/s/xLOTfURQ4EC9jmYQjFob/how-to-get-role-id-in-discord'
+              target='_blank'
+            >
+              How to invite ?
+            </a>
+          </div>
+          <div>
+            <a
+              href={dcBotLink}
+              target='_blank'
+              className='hover:text-white bg-clip-text	text-transparent bg-gradient-to-l  from-[#904BF6] to-[#CF0063] relative after:h-px after:absolute after:-bottom-1 after:inset-x-0 after:bg-gradient-to-r  after:from-[#904BF6] after:to-[#CF0063]'
+            >
+              Invite bot
+            </a>
+          </div>
+          <FormItem
+            label='Discord Server URL'
+            name={[name, 'link']}
+            rules={[
+              {
+                required: true,
+                message: 'Please input the Discord Server URL',
+              },
+              {
+                required: false,
+                message: 'Please input a valid url',
+                type: 'url',
+              },
+            ]}
+          >
+            <Input placeholder='https://discord.gg/xxxx!' />
+          </FormItem>
+        </div>
       );
     },
     pick: ['link'],
@@ -129,15 +155,29 @@ export default {
     render: ({ name }) => {
       return (
         <div className='space-y-3'>
-          <div className='text-sm font-medium text-c-9'>
+          <div className='text-sm font-medium text-c-9 flex justify-between'>
+            <div>
+              <span className='text-[#FF5151] me-1'>*</span>
+              Invite Tbook Bot as an admin to your server
+            </div>
             <a
-              className='underline text-[#1D9BF0]'
+              className='underline text-[#717374] hover:opacity-70 hover:text-white hover:underline'
               href='https://app.gitbook.com/o/XmLEuzCUK0IIbhY5X44k/s/xLOTfURQ4EC9jmYQjFob/how-to-get-role-id-in-discord'
               target='_blank'
             >
-              How to get Role ID in Discord
+              How to invite ?
             </a>
           </div>
+          <div>
+            <a
+              href={dcBotLink}
+              target='_blank'
+              className='hover:text-white bg-clip-text	text-transparent bg-gradient-to-l  from-[#904BF6] to-[#CF0063] relative after:h-px after:absolute after:-bottom-1 after:inset-x-0 after:bg-gradient-to-r  after:from-[#904BF6] after:to-[#CF0063]'
+            >
+              Invite bot
+            </a>
+          </div>
+
           <FormItem
             label='Discord Server URL'
             name={[name, 'link']}
