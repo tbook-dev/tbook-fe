9b977334741567c07253e5cedb4d802751231580
lake <posuihushui@icloud.com>
2024-05-14 21:58:21 +0800

    feat: update basic functions

--- app/campaign/src/layout/fixed/Header.jsx
+++ app/campaign/src/layout/fixed/Header.jsx
@@ -1,29 +1,29 @@
-import { Link } from 'react-router-dom'
-import logo from '@/images/icon/logo.svg'
-import clsx from 'clsx'
-import { useResponsive } from 'ahooks'
-import { useSelector } from 'react-redux'
-import Links from '../common/Links'
-import MobleMenu from '../common/MobleMenu'
-import useUserInfo from '@/hooks/useUserInfoQuery'
-import { useDispatch } from 'react-redux'
-import { setLoginModal } from '@/store/global'
-import { useAccount } from 'wagmi'
-import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { Link } from 'react-router-dom';
+import logo from '@/images/icon/logo.svg';
+import clsx from 'clsx';
+import { useResponsive } from 'ahooks';
+import { useSelector } from 'react-redux';
+import Links from '../common/Links';
+import MobleMenu from '../common/MobleMenu';
+import useUserInfo from '@/hooks/useUserInfoQuery';
+import { useDispatch } from 'react-redux';
+import { setLoginModal } from '@/store/global';
+import { useAccount } from 'wagmi';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
 // import UserAddress from '../common/UserAddress'
-import AvatarSkeleton from '../common/AvatarSkeleton'
-import Avatar from '../common/Avatar'
+import AvatarSkeleton from '../common/AvatarSkeleton';
+import Avatar from '../common/Avatar';
 
 function Header () {
-  const { pc } = useResponsive()
-  const { isConnected } = useAccount()
-  const headerTransparent = useSelector(s => s.global.headerTransparent)
-  const { userLogined, firstLoad, user } = useUserInfo()
-  const dispath = useDispatch()
-  const { open } = useWeb3Modal()
+  const { pc } = useResponsive();
+  const { isConnected } = useAccount();
+  const headerTransparent = useSelector(s => s.global.headerTransparent);
+  const { userLogined, firstLoad, user } = useUserInfo();
+  const dispath = useDispatch();
+  const { open } = useWeb3Modal();
   const handleClick = () => {
-    dispath(setLoginModal(true))
-  }
+    dispath(setLoginModal(true));
+  };
   return (
     <header
       className={clsx(
@@ -40,16 +40,11 @@ function Header () {
             </Link>
           </div>
 
-          <Links hidden={!pc} />
-
           <div>
             {!firstLoad ? (
               <AvatarSkeleton />
             ) : userLogined ? (
-              <div className='flex items-center gap-x-2'>
-                <Avatar />
-                <MobleMenu />
-              </div>
+              <Avatar />
             ) : (
               <button
                 className='px-2 py-1 text-sm rounded-md border border-white text-white lg:hover:opacity-70'
@@ -62,7 +57,7 @@ function Header () {
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- app/campaign/src/pages/home/index.jsx
+++ app/campaign/src/pages/home/index.jsx
@@ -1,12 +1,51 @@
-import Banner from './modules/banner'
-import Campaigns from './modules/campaigns'
+import pageConf from './pageConf';
+import { useResponsive } from 'ahooks';
+import { FloatButton } from 'antd';
 
 export default function Home () {
-  return (
-    <main>
-      <Banner />
+  const { pc } = useResponsive();
 
-      <Campaigns />
+  return (
+    <main
+      className='w-dvw flex-auto flex flex-col justify-end bg-cover gap-y-[180px]'
+      style={{
+        backgroundImage: `url(${pc ? pageConf.pic : pageConf.picMoble})`,
+      }}
+    >
+      <div className='lg:w-[1200px] mx-auto text-[#e1cdff]/90'>
+        {pc ? (
+          pageConf.title.map(p => (
+            <h3 className='text-[60px] leading-[80px]'>{p}</h3>
+          ))
+        ) : (
+          <h3 className='text-4xl w-[270px] mx-auto text-center'>
+            {pageConf.title.join(' ')}
+          </h3>
+        )}
+      </div>
+      <div className='lg:w-[1200px] lg:gap-x-8 lg:divide-x lg:divide-white/40 mx-auto flex items-center justify-center gap-x-10 lg:justify-start lg:pb-[130px]'>
+        {pageConf.contacts.map(c => {
+          return (
+            <a
+              key={c.name}
+              className='group flex items-center gap-x-3 lg:px-8 lg:first:pl-0'
+              target='_blank'
+              href={c.url}
+            >
+              {c.svg}
+              <div className='hidden lg:block text-xs'>
+                <p className='text-white/60 group-hover:text-white'>{c.name}</p>
+                <p className='text-white/40 group-hover:text-white'>
+                  {c.linkText}
+                </p>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+      <div className='fixed bottom-[150px] right-6 lg:bottom-16 lg:right-16 cursor-pointer group'>
+        <img src={pageConf.tipImg} />
+      </div>
     </main>
-  )
+  );
 }
