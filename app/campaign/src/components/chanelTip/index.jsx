7d0e98f4498218913d8daed740dbbf6bec793492
lake <posuihushui@icloud.com>
2024-05-15 21:02:14 +0800

    fix: fix open

--- app/campaign/src/components/chanelTip/index.jsx
+++ app/campaign/src/components/chanelTip/index.jsx
@@ -10,12 +10,12 @@ const moduleConf = {
 export default function ChanelTip () {
   return (
     <Popover className='group relative'>
-      {({ open }) => (
+      {({ open, close }) => (
         <>
           <Popover.Button
             className={clsx(
               open && 'invisible',
-              'active:outline-none focus-within:outline-none'
+              'active:outline-none focus-within:outline-none size-12'
             )}
           >
             <img
@@ -35,7 +35,7 @@ export default function ChanelTip () {
           >
             <Popover.Panel
               anchor='bottom'
-              className='absolute -translate-x-[calc(100%_-_70px)] -translate-y-full flex flex-col backdrop-blur-md'
+              className='absolute  -translate-x-[calc(100%_-_50px)] lg:-translate-x-[calc(100%_-_70px)] -translate-y-full flex flex-col backdrop-blur-md'
             >
               <div className='flex gap-x-3 border-white/20  border shadow-lg w-[300px] lg:w-[340px] p-4 rounded-2xl bg-white/5 '>
                 <svg
@@ -44,7 +44,8 @@ export default function ChanelTip () {
                   viewBox='0 0 20 20'
                   fill='none'
                   xmlns='http://www.w3.org/2000/svg'
-                  className='flex-none'
+                  className='flex-none cursor-pointer'
+                  onClick={close}
                 >
                   <path
                     d='M4.375 3.75L10.625 10L4.375 16.25M9.375 3.75L15.625 10L9.375 16.25'
@@ -57,28 +58,13 @@ export default function ChanelTip () {
                 <div className='text-sm'>
                   <p className='text-white'>{moduleConf.message}</p>
                   <a
-                    className='underline text-[#A1A1A2] hover:text-white'
                     href={moduleConf.link}
                     target='_blank'
+                    className='underline text-[#A1A1A2] hover:text-white'
                   >
                     {moduleConf.link}
                   </a>
                 </div>
-                <svg
-                  width='20'
-                  height='20'
-                  viewBox='0 0 20 20'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='flex-none'
-                >
-                  <path
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d='M6.87503 3.125H16.25C16.4158 3.125 16.5748 3.19085 16.692 3.30806C16.8092 3.42527 16.875 3.58424 16.875 3.75V13.125C16.875 13.2908 16.8092 13.4497 16.692 13.5669C16.5748 13.6842 16.4158 13.75 16.25 13.75C16.0843 13.75 15.9253 13.6842 15.8081 13.5669C15.6909 13.4497 15.625 13.2908 15.625 13.125V5.25833L4.1917 16.6917C4.07322 16.8021 3.91652 16.8622 3.7546 16.8593C3.59268 16.8565 3.43819 16.7909 3.32368 16.6764C3.20917 16.5618 3.14358 16.4074 3.14072 16.2454C3.13787 16.0835 3.19797 15.9268 3.30837 15.8083L14.7417 4.375H6.87503C6.70927 4.375 6.5503 4.30915 6.43309 4.19194C6.31588 4.07473 6.25003 3.91576 6.25003 3.75C6.25003 3.58424 6.31588 3.42527 6.43309 3.30806C6.5503 3.19085 6.70927 3.125 6.87503 3.125Z'
-                    fill='white'
-                  />
-                </svg>
               </div>
             </Popover.Panel>
           </Transition>
