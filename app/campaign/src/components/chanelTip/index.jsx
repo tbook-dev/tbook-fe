91a852545badf1afa75f0eb77ddf47bb1c092c05
lake <posuihushui@icloud.com>
2024-05-15 15:28:35 +0800

    fix: fix animation

--- app/campaign/src/components/chanelTip/index.jsx
+++ app/campaign/src/components/chanelTip/index.jsx
@@ -1,6 +1,5 @@
 import tipImg from '@/images/tip.svg';
 import { Popover, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
 import clsx from 'clsx';
 
 const moduleConf = {
@@ -33,7 +32,6 @@ export default function ChanelTip () {
             leave='transition ease-in duration-150'
             leaveFrom='opacity-100 translate-y-0'
             leaveTo='opacity-0 translate-y-1'
-            as={Fragment}
           >
             <Popover.Panel
               anchor='bottom'
