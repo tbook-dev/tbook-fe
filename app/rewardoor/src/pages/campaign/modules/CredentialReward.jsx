bfd56cf8336fd85f971929f4b15f6b7f96363d04
lake <posuihushui@icloud.com>
2024-05-20 17:11:21 +0800

    feat: update onging campign edit

--- app/rewardoor/src/pages/campaign/modules/CredentialReward.jsx
+++ app/rewardoor/src/pages/campaign/modules/CredentialReward.jsx
@@ -7,10 +7,9 @@ import { PlusOutlined, CloseOutlined } from '@ant-design/icons';
 import CredentialModal from './CredentialModal';
 import RewardModal from './RewardModal';
 import { incentiveAssetsTypeList } from '@/utils/conf';
-import x from '@/images/icon/x.svg';
 import { defaultCredentialReward } from '../conf';
-import { template } from 'lodash';
 import { Display } from '@tbook/credential';
+import clsx from 'clsx';
 
 const textConf = {
   credential: {
@@ -29,54 +28,59 @@ const addText = 'Add Credential Group & Reward';
 const editCredentialText = 'Edit Credential Group';
 const editRewardText = 'Edit Rewards';
 
-function CredentialReward({
+function CredentialReward ({
   credentialReward,
   setCredentialReward,
   NFTcontracts,
   credentialList,
+  isInOngoingEdit,
 }) {
-  const credentialSet = credentialList.map((v) => v.credentialList).flat();
-
   const [editCredentialIndex, setEditCredentialIndex] = useState(0);
   const [showCredentialModal, setShowCredentialModal] = useState(false);
   const [showRewardModal, setShowRewardModal] = useState(false);
   const [editRewardIndex, setEditRewardIndex] = useState(0);
 
-  const handleDelete = (c) => {
+  const handleDelete = c => {
     setCredentialReward(credentialReward.filter((_, idx) => idx !== c));
   };
   return (
     <div>
-      <div className="grid grid-cols-2 space-x-5 mb-5">
+      <div className='grid grid-cols-2 space-x-5 mb-5'>
         {[textConf.credential, textConf.reward].map((item, index) => {
           return (
-            <div key={index} className="font-medium">
-              <h3 className="text-base font-bold text-t-1">{item.title}</h3>
-              <p className="mt-0.5 text-c-9 text-xs">{item.desc}</p>
+            <div key={index} className='font-medium'>
+              <h3 className='text-base font-bold text-t-1'>{item.title}</h3>
+              <p className='mt-0.5 text-c-9 text-xs'>{item.desc}</p>
             </div>
           );
         })}
       </div>
 
-      <div className="space-y-5">
+      <div className='space-y-5'>
         {credentialReward.map((cr, index, list) => {
           return (
             <div
-              className="text-white py-5 px-12 bg-gray rounded-2.5xl grid grid-cols-2 gap-x-10 relative before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:w-[1px] before:h-10 before:bg-c-6"
+              className='text-white py-5 px-12 bg-gray rounded-2.5xl grid grid-cols-2 gap-x-10 relative before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:w-[1px] before:h-10 before:bg-c-6'
               key={index}
             >
               {list.length > 1 && (
                 <span
-                  className="absolute top-1/2 transform -translate-y-1/2 right-5 cursor-pointer"
-                  onClick={() => handleDelete(index)}
+                  className={clsx(
+                    isInOngoingEdit ? 'cursor-not-allowed	' : 'cursor-pointer',
+                    'absolute top-1/2 transform -translate-y-1/2 right-5'
+                  )}
+                  onClick={() => {
+                    if (isInOngoingEdit) return;
+                    handleDelete(index);
+                  }}
                 >
                   <CloseOutlined style={{ color: '#999' }} />
                 </span>
               )}
 
-              <div className="flex items-center w-full">
+              <div className='flex items-center w-full'>
                 {cr.credential.length > 0 ? (
-                  <div className="space-y-6 w-full">
+                  <div className='space-y-6 w-full'>
                     {cr.credential.map((v, idx) => {
                       return (
                         <Display
@@ -88,8 +92,14 @@ function CredentialReward({
                       );
                     })}
                     <p
-                      className="text-c-9 underline font-medium text-xs cursor-pointer"
+                      className={clsx(
+                        isInOngoingEdit
+                          ? 'cursor-not-allowed	'
+                          : 'cursor-pointer',
+                        'text-c-9 underline font-medium text-xs'
+                      )}
                       onClick={() => {
+                        if (isInOngoingEdit) return;
                         setEditCredentialIndex(index);
                         setShowCredentialModal(true);
                       }}
@@ -99,7 +109,7 @@ function CredentialReward({
                   </div>
                 ) : (
                   <div
-                    className="py-[30px] w-full	text-center bg-cover bg-no-repeat bg-center cursor-pointer"
+                    className='py-[30px] w-full	text-center bg-cover bg-no-repeat bg-center cursor-pointer'
                     style={{ backgroundImage: `url(${credentialCreatepng})` }}
                     onClick={() => {
                       setEditCredentialIndex(index);
@@ -108,30 +118,30 @@ function CredentialReward({
                   >
                     <img
                       src={editIcon}
-                      className="inline w-3 h-3 mr-3 cursor-pointer"
+                      className='inline w-3 h-3 mr-3 cursor-pointer'
                     />
                     {credentialPrompt}
                   </div>
                 )}
               </div>
 
-              <div className="flex items-center">
+              <div className='flex items-center'>
                 {cr.reward.length > 0 ? (
-                  <div className="space-y-6 w-full">
-                    <div className="space-y-6">
+                  <div className='space-y-6 w-full'>
+                    <div className='space-y-6'>
                       {cr.reward.map((v, idx) => {
                         const rewardType = v.rewardType;
                         const reward = incentiveAssetsTypeList.find(
-                          (i) => i.value === rewardType
+                          i => i.value === rewardType
                         );
 
                         return (
                           <div
                             key={idx}
-                            className="px-6 py-2 text-xs font-medium text-t-1 border border-c-6 rounded-2.5xl flex justify-between items-center"
+                            className='px-6 py-2 text-xs font-medium text-t-1 border border-c-6 rounded-2.5xl flex justify-between items-center'
                           >
-                            <span className="inline-flex items-center gap-x-1">
-                              <img src={reward?.icon} className="w-6 h-6" />
+                            <span className='inline-flex items-center gap-x-1'>
+                              <img src={reward?.icon} className='w-6 h-6' />
                               {reward?.text}
                             </span>
 
@@ -141,8 +151,14 @@ function CredentialReward({
                       })}
                     </div>
                     <p
-                      className="text-c-9 underline font-medium text-xs cursor-pointer"
+                      className={clsx(
+                        isInOngoingEdit
+                          ? 'cursor-not-allowed	'
+                          : 'cursor-pointer',
+                        'text-c-9 underline font-medium text-xs'
+                      )}
                       onClick={() => {
+                        if (isInOngoingEdit) return;
                         setEditRewardIndex(index);
                         setShowRewardModal(true);
                       }}
@@ -151,19 +167,19 @@ function CredentialReward({
                     </p>
                   </div>
                 ) : cr.credential.length === 0 ? (
-                  <div className="py-[30px] text-c-6 w-full flex items-center justify-center">
+                  <div className='py-[30px] text-c-6 w-full flex items-center justify-center'>
                     {rewardPrompt}
                   </div>
                 ) : (
                   <div
-                    className="py-[30px] w-full flex items-center justify-center bg-cover bg-no-repeat bg-center cursor-pointer"
+                    className='py-[30px] w-full flex items-center justify-center bg-cover bg-no-repeat bg-center cursor-pointer'
                     style={{ backgroundImage: `url(${rewardCreatepng})` }}
                     onClick={() => {
                       setEditRewardIndex(index);
                       setShowRewardModal(true);
                     }}
                   >
-                    <img src={editIcon} className="inline w-3 h-3 mr-3" />
+                    <img src={editIcon} className='inline w-3 h-3 mr-3' />
                     {rewardReadyPrompt}
                   </div>
                 )}
@@ -180,7 +196,7 @@ function CredentialReward({
         credentialList={credentialList}
         open={showCredentialModal}
         setOpen={setShowCredentialModal}
-        handleSave={(values) => {
+        handleSave={values => {
           if (values) {
             setCredentialReward(
               credentialReward.map((v, idx) => {
@@ -201,7 +217,7 @@ function CredentialReward({
         NFTcontracts={NFTcontracts}
         open={showRewardModal}
         setOpen={setShowRewardModal}
-        handleSave={(values) => {
+        handleSave={values => {
           if (values) {
             setCredentialReward(
               credentialReward.map((v, idx) => {
@@ -214,10 +230,14 @@ function CredentialReward({
           }
         }}
       />
-      <div className="pt-5">
+      <div className='pt-5'>
         <Button
-          type="text"
-          className="px-0 -ml-10"
+          type='text'
+          className={clsx(
+            isInOngoingEdit ? 'cursor-not-allowed	' : 'cursor-pointer',
+            'px-0 -ml-10'
+          )}
+          // disabled={isInOngoingEdit}
           onClick={() => {
             setCredentialReward([
               ...credentialReward,
@@ -225,7 +245,7 @@ function CredentialReward({
             ]);
           }}
         >
-          <PlusOutlined className="mr-2" />
+          <PlusOutlined className='mr-2' />
           {addText}
         </Button>
       </div>
