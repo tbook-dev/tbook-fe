3aef69300e849cbb04bdf0d943c93e218e35f81c
lake <posuihushui@icloud.com>
2024-05-20 17:22:26 +0800

    fix scale

--- app/campaign/src/components/withVerify/index.jsx
+++ app/campaign/src/components/withVerify/index.jsx
@@ -1,12 +1,12 @@
-import { useState, useCallback } from 'react'
-import clsx from 'clsx'
-import useSocial from '@/hooks/useSocial'
-import VerifyStatus, { verifyStatusEnum } from './VerifyStatus'
-import useUserInfo from '@/hooks/useUserInfoQuery'
-import { useDispatch } from 'react-redux'
-import { setLoginModal, setConnectWalletModal } from '@/store/global'
-import { Modal } from 'antd'
-import { useResponsive } from 'ahooks'
+import { useState, useCallback } from 'react';
+import clsx from 'clsx';
+import useSocial from '@/hooks/useSocial';
+import VerifyStatus, { verifyStatusEnum } from './VerifyStatus';
+import useUserInfo from '@/hooks/useUserInfoQuery';
+import { useDispatch } from 'react-redux';
+import { setLoginModal, setConnectWalletModal } from '@/store/global';
+import { Modal } from 'antd';
+import { useResponsive } from 'ahooks';
 
 const modalConf = {
   title: 'Verify',
@@ -15,34 +15,39 @@ const modalConf = {
     desc: {
       twitter: 'Please authorize your X account and continue to verify.',
       discord: 'Please authorize your Discord account and continue to verify.',
-      telegram: 'Please authorize your Telegram account and continue to verify.'
-    }
-  }
-}
+      telegram:
+        'Please authorize your Telegram account and continue to verify.',
+    },
+  },
+};
 
-export default function WithVerify ({ handleFn, evmRequire, count, credentialType }) {
-  const { pc } = useResponsive()
+export default function WithVerify ({
+  handleFn,
+  evmRequire,
+  count,
+  credentialType,
+}) {
+  const { pc } = useResponsive();
   // const open = useSelector(v => v.global.showSocicalModal)
-  const [open, setOpen] = useState(false)
-  const { getSocialByName } = useSocial()
-  const [status, setStatus] = useState(verifyStatusEnum.NotStarted)
-  const { userLogined, wallectConnected } = useUserInfo()
-  const dispath = useDispatch()
-  const social = getSocialByName(credentialType)
-  const isSocial = !!social
+  const [open, setOpen] = useState(false);
+  const { getSocialByName } = useSocial();
+  const [status, setStatus] = useState(verifyStatusEnum.NotStarted);
+  const { userLogined, wallectConnected } = useUserInfo();
+  const dispath = useDispatch();
+  const social = getSocialByName(credentialType);
+  const isSocial = !!social;
   const handleVerify = async evt => {
-    setStatus(verifyStatusEnum.Pending)
+    setStatus(verifyStatusEnum.Pending);
     try {
-      await handleFn(evt)
-      setStatus(verifyStatusEnum.Sucess)
+      await handleFn(evt);
+      setStatus(verifyStatusEnum.Sucess);
     } catch (e) {
-      setStatus(verifyStatusEnum.NotStarted)
+      setStatus(verifyStatusEnum.NotStarted);
     }
-  }
+  };
   const handleCancel = useCallback(() => {
-    setOpen(false)
-  }, [])
-
+    setOpen(false);
+  }, []);
 
   return (
     <>
@@ -50,15 +55,15 @@ export default function WithVerify ({ handleFn, evmRequire, count, credentialTyp
         disabled={status === verifyStatusEnum.Pending || count > 0}
         className={clsx(
           'text-base whitespace-nowrap px-1.5 py-1 rounded',
-          'flex items-center gap-x-1',
+          'flex-none flex items-center gap-x-1',
           {
             'bg-transparent': [
               verifyStatusEnum.Sucess,
-              verifyStatusEnum.Pending
+              verifyStatusEnum.Pending,
             ].includes(status),
             'text-white bg-[#904BF6]': status === verifyStatusEnum.NotStarted,
             'bg-[#2B174A] text-[#55456E] w-[78px] justify-center cursor-not-allowed':
-              count > 0
+              count > 0,
           }
         )}
         onClick={evt => {
@@ -75,15 +80,15 @@ export default function WithVerify ({ handleFn, evmRequire, count, credentialTyp
           //   }
           // }
           if (!userLogined) {
-            dispath(setLoginModal(true))
-          } else if(evmRequire && !wallectConnected){
-            dispath(setConnectWalletModal(true))
-          }else{
+            dispath(setLoginModal(true));
+          } else if (evmRequire && !wallectConnected) {
+            dispath(setConnectWalletModal(true));
+          } else {
             if (isSocial && !social.connected) {
               // dispath(setShowSocicalModal(true))
-              setOpen(true)
+              setOpen(true);
             } else {
-              handleVerify(evt)
+              handleVerify(evt);
             }
           }
         }}
@@ -122,7 +127,7 @@ export default function WithVerify ({ handleFn, evmRequire, count, credentialTyp
 
               <button
                 onClick={() => {
-                  social.loginFn(false)
+                  social.loginFn(false);
                 }}
                 className='flex items-center gap-x-1 px-4 py-1 text-sm font-medium text-black rounded-md bg-white'
               >
@@ -138,5 +143,5 @@ export default function WithVerify ({ handleFn, evmRequire, count, credentialTyp
         </Modal>
       )}
     </>
-  )
+  );
 }
