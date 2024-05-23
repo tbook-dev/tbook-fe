439518bf11ff8bb0312fca0466c714d2d1e3be92
lake <posuihushui@icloud.com>
2024-05-23 16:40:07 +0800

    feat: add wise-score tma share link

--- app/campaign/src/layout/common/Footer.jsx
+++ app/campaign/src/layout/common/Footer.jsx
@@ -13,21 +13,23 @@ export default function Footer () {
   const { pc } = useResponsive();
 
   return (
-    <footer className='bx text-xs text-center text-white/60 border-t border-white/10'>
-      {showFooterTip && (
-        <h5 className={clsx('py-2', pc ? 'flex justify-center gap-x-1' : '')}>
-          <p>{chanelTips[0]}</p>
-          <a
-            href={chanelTips[1]}
-            target='_blank'
-            className='underline hover:text-white transition-all'
-          >
-            {chanelTips[1]}
-          </a>
-        </h5>
-      )}
+    <footer className='w-full'>
+      <div className='bx text-xs text-center text-white/60 border-t border-white/10'>
+        {showFooterTip && (
+          <h5 className={clsx('py-2', pc ? 'flex justify-center gap-x-1' : '')}>
+            <p>{chanelTips[0]}</p>
+            <a
+              href={chanelTips[1]}
+              target='_blank'
+              className='underline hover:text-white transition-all'
+            >
+              {chanelTips[1]}
+            </a>
+          </h5>
+        )}
 
-      <p className='py-3'>{footText}</p>
+        <p className='py-3'>{footText}</p>
+      </div>
     </footer>
   );
 }
