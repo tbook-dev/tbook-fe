3ab4a853e901af4fca7ca7a50afd6f2ac7bdefcf
lake <posuihushui@icloud.com>
2024-06-10 11:27:07 +0800

    add leadboard

--- app/campaign/src/pages/renaissance/components/leaderboard.jsx
+++ app/campaign/src/pages/renaissance/components/leaderboard.jsx
@@ -1,13 +1,19 @@
 import useUserRenaissance from '@/hooks/useUserRenaissance';
 import useWiseScore from '@/hooks/useWiseScore';
+import useTopBoard from '@/hooks/useTopBoard';
 import UserScore from '@/pages/ton-wise/components/userScore';
 import Top from './ui/top';
+import { cn } from '@/utils/conf';
 
 export default function Leaderboard () {
   const { isLoading, data } = useUserRenaissance();
   const { data: userScore, isLoading: isUserScoreLoading } = useWiseScore();
-  console.log({ userScore });
+  const { data: top } = useTopBoard();
   const friends = data?.friends ?? [];
+  const top1 = top?.[0];
+  const top2 = top?.[1];
+  const top3 = top?.[2];
+  console.log({ top3 });
   return (
     <div className='p-4 border border-[#8541EE]/70 rounded-2xl space-y-5'>
       <h2 className='text-lg font-syne font-bold text-[#FFDFA2]'>
@@ -16,8 +22,35 @@ export default function Leaderboard () {
 
       <UserScore user={userScore} type={2} />
 
-      <div className='flex -space-x-1.5'>
-        <Top />
+      <div
+        className={cn('flex px-4', top3 ? 'justify-between' : 'justify-around')}
+      >
+        {top2 && (
+          <Top
+            rank={2}
+            className='mt-10'
+            score={top2?.totalScore ?? 0}
+            address={top2?.address}
+            avatar={top2?.avatar}
+          />
+        )}
+
+        <Top
+          rank={1}
+          score={top1?.totalScore ?? 0}
+          address={top1?.address}
+          avatar={top1?.avatar}
+        />
+
+        {top3 && (
+          <Top
+            rank={3}
+            className='mt-12'
+            score={top3?.totalScore ?? 0}
+            address={top3?.address}
+            avatar={top3?.avatar}
+          />
+        )}
       </div>
     </div>
   );
--- app/campaign/src/pages/renaissance/components/ui/top.jsx
+++ app/campaign/src/pages/renaissance/components/ui/top.jsx
@@ -1,26 +1,38 @@
 import clsx from 'clsx';
 import { shortAddressV1, formatImpact } from '@tbook/utils/lib/conf';
 
-export default function BoardTop ({ rank = 1, className, address, score }) {
-  const clsxMap = {
-    1: 'size-[100px]',
-    2: 'size-20',
-    3: 'size-20',
-  };
-
+export default function BoardTop ({
+  rank = 1,
+  className,
+  address,
+  score,
+  avatar,
+}) {
   return (
-    <div className={clsx('space-y-4', className)}>
+    <div className={clsx('space-y-4 flex flex-col items-center', className)}>
       <div
         className={clsx(
-          clsxMap[rank],
-          'ring-2 ring-[#9E78D6] relative rounded-full'
+          'ring-2 ring-[#FFDFA2]  rounded-full relative',
+          rank === 1 ? 'size-16' : 'size-12'
         )}
+        style={{ backgroundImage: `url(${avatar})` }}
       >
-        xxx
+        <span
+          className={clsx(
+            'absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 bg-[#E59A40] text-xs rounded-full flex items-center justify-center text-black',
+            rank === 1 ? 'size-[22px]' : 'size-[19px]'
+          )}
+        >
+          {rank}
+        </span>
       </div>
-      <div className='text-sm text-center'>
-        <p className='font-medium'>{shortAddressV1(address ?? '')}</p>
-        <p className='font-zen-dot text-[#FFB800]'>{formatImpact(score)}</p>
+      <div className='text-center'>
+        <p className='font-bold text-[10px] text-[#F8C685]'>
+          {shortAddressV1(address ?? '')}
+        </p>
+        <p className='font-zen-dot text-[6px] text-white'>
+          {formatImpact(score)}
+        </p>
       </div>
     </div>
   );
