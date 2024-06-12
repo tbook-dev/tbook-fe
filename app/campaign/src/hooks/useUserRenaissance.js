49b48b6b21ec436efb7d9cc7a1d384ab3919d6d3
lake <posuihushui@icloud.com>
2024-06-13 06:56:20 +0800

    fix link

--- app/campaign/src/hooks/useUserRenaissance.js
+++ app/campaign/src/hooks/useUserRenaissance.js
@@ -102,7 +102,7 @@ export const useUserRenaissanceKit = () => {
     const text = `@${TG_BOT_NAME} Hi friend, 💅click to get your lucky cards. 🎉 \n ${link}\n🔥 The thrilling scratch competition is now in full bloom! 💥\n🎁 Prize Pool: 💰NOTCoin, 💲20,000U`;
     const shareLink = `https://t.me/share/url?text=${encodeURIComponent(
       text
-    )}&link=${encodeURIComponent(link)}`;
+    )}&url=${encodeURIComponent(link)}`;
     WebApp.openTelegramLink(shareLink);
   }, [userId]);
 
