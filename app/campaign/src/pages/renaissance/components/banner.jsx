1dc12de244d142b37f04469f90635fed3626c477
lake <posuihushui@icloud.com>
2024-06-12 16:44:26 +0800

    feat: add rules drawer

--- app/campaign/src/pages/renaissance/components/banner.jsx
+++ app/campaign/src/pages/renaissance/components/banner.jsx
@@ -1,13 +1,31 @@
 import moduleConf from '../conf';
+import { useState } from 'react';
+import RuleModal from './modal/rule';
 
 export default function Banner () {
-  return (
-    <div className='flex justify-center relative'>
-      {moduleConf.svg.renaissanceText}
+  const [isRuleModalOpen, setIsRuleModalOpen] = useState(false);
 
-      <button className='absolute -right-5 bottom-[34px] button py-1.5 px-3 text-sm rounded-l-lg border border-r-0 lg:border-r lg:rounded-r-lg border-[#FFDFA2]/60  text-[#f8c685]/60'>
-        Rules
-      </button>
-    </div>
+  return (
+    <>
+      <div className='flex justify-center relative'>
+        {moduleConf.svg.renaissanceText}
+
+        <button
+          onClick={() => {
+            setIsRuleModalOpen(true);
+          }}
+          className='absolute -right-5 bottom-[34px] button py-1.5 px-3 text-sm rounded-l-lg border border-r-0 lg:border-r lg:rounded-r-lg border-[#FFDFA2]/60  text-[#f8c685]/60'
+        >
+          Rules
+        </button>
+      </div>
+
+      <RuleModal
+        open={isRuleModalOpen}
+        onCancel={() => {
+          setIsRuleModalOpen(false);
+        }}
+      />
+    </>
   );
 }
