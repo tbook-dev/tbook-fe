eb2f6af7b1d12cc54f6d10dcfdb7490874df0a89
lake <posuihushui@icloud.com>
2024-07-03 07:33:05 +0800

    update social ui

--- app/campaign/src/pages/ton-wise/wise-detail/Social.jsx
+++ app/campaign/src/pages/ton-wise/wise-detail/Social.jsx
@@ -37,99 +37,120 @@ export default function Social () {
   const actionList = [
     {
       type: 'telegram',
+      show: true,
       actionName: 'Telegram AllStar Stats',
       handle: () => {
         setTg(true);
       },
+      list: [
+        {
+          picUrl: 'https://campaign-staging.tbook.com/logo.svg',
+          title: `${formatImpact(1000)} Fans`,
+          extralInfo: true ? 'Telegram Channel' : 'Telegram Group',
+          id: 1,
+        },
+        {
+          picUrl: 'https://campaign-staging.tbook.com/logo.svg',
+          title: `${formatImpact(1000)} Fans`,
+          extralInfo: true ? 'Telegram Channel' : 'Telegram Group',
+          id: 2,
+        },
+        {
+          picUrl: 'https://campaign-staging.tbook.com/logo.svg',
+          title: `${formatImpact(1000)} Fans`,
+          extralInfo: true ? 'Telegram Channel' : 'Telegram Group',
+          id: 3,
+        },
+        {
+          picUrl: 'https://campaign-staging.tbook.com/logo.svg',
+          title: `${formatImpact(1000)} Fans`,
+          extralInfo: true ? 'Telegram Channel' : 'Telegram Group',
+          id: 4,
+        },
+      ],
+    },
+    {
+      type: 'twitter',
+      show: '',
+      actionName: 'Twitter AllStar Stats',
+      handle: null,
+      list: [
+        {
+          picUrl: 'https://campaign-staging.tbook.com/logo.svg',
+          title: `${formatImpact(1000)} Followers`,
+          extralInfo: null,
+          id: 1,
+        },
+      ],
     },
     {
       type: 'discord',
+      show: false,
       actionName: 'Discord AllStar Stats',
       handle: () => {
         setDc(true);
       },
+      list: [
+        {
+          picUrl: 'https://campaign-staging.tbook.com/logo.svg',
+          title: null,
+          extralInfo: 'Discord Server',
+          id: 1,
+        },
+        {
+          picUrl: 'https://campaign-staging.tbook.com/logo.svg',
+          title: null,
+          extralInfo: 'Discord Server',
+          id: 2,
+        },
+      ],
     },
   ];
-  const recordList = data?.userDcTgShareLink ?? [];
+
   return (
     <>
       {contextHolder}
       <Telegram open={tgOpen} onClose={handleTgClose} mutation={mutation} />
       <Discord open={dcOpen} onClose={handleDcClose} mutation={mutation} />
-      <div className='pt-6 space-y-16'>
-        <div className='space-y-5'>
-          <h2 className='text-base font-zen-dot'>{modlueConf.title}</h2>
 
-          <div className='flex flex-col gap-y-4'>
-            {actionList.map(v => {
-              return (
-                <button
-                  key={v.type}
-                  className='bg-gradient-to-br from-purple-500 to-pink-500 p-px flex font-medium text-sm rounded-lg btn-click'
-                  onClick={v.handle}
-                >
-                  <span className='bg-[#0E0819] flex items-center justify-between flex-1 p-4 gap-x-[30px] rounded-lg'>
-                    <span className='text-[#904BF6] text-left'>
-                      Submit
-                      <span className='text-white ms-1'>
-                        {modlueConf.acionMap[v.type]}
-                      </span>
-                    </span>
-                    <svg
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M13.5 4.5L21 12M21 12L13.5 19.5M21 12H3'
-                        stroke='#A1A1AA'
-                        strokeWidth='1.5'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                      />
-                    </svg>
-                  </span>
-                </button>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className='space-y-5'>
-          <h2 className='text-base font-zen-dot'>{modlueConf.recordTitle}</h2>
-
-          <div className='flex flex-col gap-y-4'>
-            {recordList.length > 0 ? (
-              recordList.map(v => {
-                return (
-                  <div
-                    key={v.socialType}
-                    className='bg-[rgb(144,75,246)]/[0.10] p-5 flex items-center justify-between gap-x-[30px] text-sm rounded-lg'
-                  >
-                    <div className='flex flex-col gap-y-0.5'>
-                      <span className='text-white text-sm font-medium'>
-                        {v.title}
-                      </span>
-                      <span className='text-[#C0ABD9] text-xs'>
-                        {modlueConf.recordMap[v.socialType]}
-                      </span>
-                    </div>
-                    <div className='text-[#C0ABD9]'>
-                      <span className='me-1 text-base font-medium text-white'>
-                        {formatImpact(v.memberCount)}
-                      </span>
-                      fan
-                      {v.memberCount > 1 && 's'}
-                    </div>
-                  </div>
-                );
-              })
-            ) : (
-              <Empty text='There’s no record yet.' />
-            )}
-          </div>
+      <div className='space-y-5'>
+        <div className='flex flex-col gap-y-4'>
+          {actionList.map(v => {
+            return (
+              <div key={v.type} className='space-y-3' onClick={v.handle}>
+                <div className='flex justify-between items-center'>
+                  <h3>{v.actionName}</h3>
+                  {v.handle && (
+                    <button className='text-white font-medium text-sm bg-[#904BF6] rounded px-3 py-1.5 btn-click'>
+                      Improve
+                    </button>
+                  )}
+                </div>
+                <div className='grid grid-cols-3 gap-3'>
+                  {v.list.map(p => {
+                    return (
+                      <div
+                        key={p.id}
+                        className='flex flex-col items-center text-xs gap-1'
+                      >
+                        <img
+                          src={p.picUrl}
+                          alt={`${v.type}-${v.extralInfo}`}
+                          className='size-10 rounded-full'
+                        />
+                        <div className='space-y-0.5 text-center'>
+                          {p.title && <h4 className='text-white'>{p.title}</h4>}
+                          {p.extralInfo && (
+                            <p className='text-white/40'>{p.extralInfo}</p>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
