c007b7d77e9aca77827e6ec0e1293ca6b770945b
lake <posuihushui@icloud.com>
2024-07-19 19:28:00 +0800

    update hide logic

--- app/campaign/src/pages/ton-wise/generating/index.jsx
+++ app/campaign/src/pages/ton-wise/generating/index.jsx
@@ -5,19 +5,28 @@ import SBT50KIcon from '@/images/icon/svgr/sbt50K.svg?react';
 import SBT200KIcon from '@/images/icon/svgr/sbt200K.svg?react';
 import SBT500KIcon from '@/images/icon/svgr/sbt500K.svg?react';
 import SBT1MIcon from '@/images/icon/svgr/sbt1M.svg?react';
+import TgPremiumIcon from '@/images/icon/svgr/tg-premium.svg?react';
+import NotcoinIcon from '@/images/icon/svgr/notcoin.svg?react';
+import TonIcon from '@/images/icon/svgr/ton.svg?react';
+import ArrowIcon from '@/images/icon/svgr/arrow2.svg?react';
+import wisescoreRadarSVG from '@/images/wise/radar.svg';
+import { formatImpact } from '@tbook/utils/lib/conf';
 import Button from '../components/button';
 import Check from './check';
 import clsx from 'clsx';
 
 export default function Generating({ data, hasWiseScoreRes, wiseTag, hide }) {
-  const isFirstGen = data?.isFirstCreate;
-  //
   const [displayIdx, setDisplayIdx] = useState(0);
   useEffect(() => {
     if (!hasWiseScoreRes) {
-      // hide();
+      hide();
     }
   }, [hasWiseScoreRes]);
+  useEffect(() => {
+    if (data?.isFirstCreate === false) {
+      hide();
+    }
+  }, [data]);
   const Loading = useCallback(({ className }) => {
     return (
       <div className={clsx(className, 'relative h-full overflow-hidden ')}>
@@ -57,28 +66,34 @@ export default function Generating({ data, hasWiseScoreRes, wiseTag, hide }) {
       {
         show: true,
         key: 'check',
-        content: ({ next }) => (
-          <div className="relative px-4 h-full pb-10 space-y-16">
-            <h2 className="text-2xl w-[220px] font-thin">
-              Checking your Incentive Passport
-            </h2>
-            <div className="flex flex-col items-center w-full gap-y-10 pb-20">
-              <div className="space-y-8 w-full">
-                <Check title="TON Address" />
-                <Check title="Telegram Account" />
-                <Check title="Twitter Account" />
-                <Check title="Discord Account" />
-                <Check title="Incentive Footprint" />
+        content: ({ next }) => {
+          useEffect(() => {
+            const timer = setTimeout(next, 3000);
+            return () => clearTimeout(timer);
+          }, []);
+          return (
+            <div className="relative px-4 h-full pb-10 space-y-16">
+              <h2 className="text-2xl w-[220px] font-thin">
+                Checking your Incentive Passport
+              </h2>
+              <div className="flex flex-col items-center w-full gap-y-10 pb-20">
+                <div className="space-y-8 w-full">
+                  <Check title="TON Address" />
+                  <Check title="Telegram Account" />
+                  <Check title="Twitter Account" />
+                  <Check title="Discord Account" />
+                  <Check title="Incentive Footprint" />
+                </div>
               </div>
+              {/* <Button
+                className="h-10 w-[310px] absolute bottom-10 inset-x-0 mx-auto"
+                onClick={next}
+              >
+                Continue
+              </Button> */}
             </div>
-            <Button
-              className="h-10 w-[310px] absolute bottom-10 inset-x-0 mx-auto"
-              onClick={next}
-            >
-              Continue
-            </Button>
-          </div>
-        ),
+          );
+        },
       },
       {
         show: true,
@@ -101,17 +116,17 @@ export default function Generating({ data, hasWiseScoreRes, wiseTag, hide }) {
         ),
       },
       {
-        show: data?.identityScore?.tgPremiumScore >= 0,
-        key: 'tgPremium',
+        show: true,
+        key: 'tg-year',
         content: ({ next }) => (
           <div className="relative px-4 h-full pb-10 flex flex-col justify-center">
             <h2 className="text-2xl w-full absolute top-0 left-0 text-center">
               You’ve joined Telegram
             </h2>
-            <div className="flex flex-col items-center w-full gap-y-10 pb-20">
+            <div className="flex flex-col items-center w-full gap-y-10">
               <div className="space-y-8 w-full flex flex-col items-center">
-                <div className="text-[160px] leading-[160px]">1</div>
-                <p className="text-xl">year ago</p>
+                <div className="text-[150px] leading-[150px]">1</div>
+                <p className="text-xl">Premium User</p>
               </div>
             </div>
             <Button
@@ -119,6 +134,127 @@ export default function Generating({ data, hasWiseScoreRes, wiseTag, hide }) {
               onClick={next}
             >
               Continue
+            </Button>
+          </div>
+        ),
+      },
+      {
+        show: data?.identityScore?.tgPremiumScore >= 0,
+        key: 'tg-premium',
+        content: ({ next }) => (
+          <div className="relative px-4 h-full pb-10 flex flex-col justify-center">
+            <h2 className="text-2xl w-full absolute top-0 left-0 text-center">
+              Telegram Premium
+            </h2>
+            <div className="flex flex-col items-center w-full gap-y-10">
+              <div className="space-y-8 w-full flex flex-col items-center">
+                <TgPremiumIcon className="size-[160px]" />
+                <p className="text-xl">Premium User</p>
+              </div>
+            </div>
+            <Button
+              className="h-10 w-[310px] mx-auto absolute inset-x-0 bottom-10"
+              onClick={next}
+            >
+              Continue
+            </Button>
+          </div>
+        ),
+      },
+      {
+        show: data?.engagementScore?.notCoinTransactionScore >= 0,
+        key: 'notcoin-liquidity-provider',
+        content: ({ next }) => (
+          <div className="relative px-4 h-full pb-10 flex flex-col justify-center">
+            <h2 className="text-2xl w-full absolute top-0 left-0 text-center">
+              Liquidity Provider
+            </h2>
+            <div className="flex flex-col items-center w-full gap-y-10">
+              <div className="space-y-8 w-full flex flex-col items-center">
+                <NotcoinIcon className="size-[180px]" />
+                <p className="text-xl">Notcoin LP</p>
+              </div>
+            </div>
+            <Button
+              className="h-10 w-[310px] mx-auto absolute inset-x-0 bottom-10"
+              onClick={next}
+            >
+              Continue
+            </Button>
+          </div>
+        ),
+      },
+      {
+        show: data?.engagementScore?.tonTransactionsScore >= 0,
+        key: 'ton-liquidity-provider',
+        content: ({ next }) => (
+          <div className="relative px-4 h-full pb-10 flex flex-col justify-center">
+            <h2 className="text-2xl w-full absolute top-0 left-0 text-center">
+              Onchain Footprint
+            </h2>
+            <div className="flex flex-col items-center w-full gap-y-10">
+              <div className="space-y-8 w-full flex flex-col items-center">
+                <TonIcon className="size-[160px]" />
+                <p className="text-xl">Token Ranger</p>
+              </div>
+            </div>
+            <Button
+              className="h-10 w-[310px] mx-auto absolute inset-x-0 bottom-10"
+              onClick={next}
+            >
+              Continue
+            </Button>
+          </div>
+        ),
+      },
+      {
+        show: data?.engagementScore?.tonTransactionsScore >= 0,
+        key: 'fans',
+        content: ({ next }) => (
+          <div className="relative px-4 h-full pb-10 flex flex-col justify-center">
+            <h2 className="text-2xl w-full absolute top-0 left-0 text-center">
+              Social Star!
+            </h2>
+            <div className="flex flex-col items-center w-full gap-y-10">
+              <div className="space-y-8 w-full flex flex-col items-center">
+                <div className="text-[150px] leading-[150px]">🌟</div>
+                <p className="text-xl">{formatImpact(10000)}</p>
+              </div>
+            </div>
+            <Button
+              className="h-10 w-[310px] mx-auto absolute inset-x-0 bottom-10"
+              onClick={next}
+            >
+              Continue
+            </Button>
+          </div>
+        ),
+      },
+      {
+        show: true,
+        key: 'wise-credit',
+        content: ({ next }) => (
+          <div className="relative px-4 h-full pb-10 flex flex-col justify-center">
+            <div className="absolute top-0 left-0 text-center  w-full">
+              <h2 className="text-2xl">You are {wiseTag}!</h2>
+              <p className="text-base"> Here is your WISE Credit Score</p>
+            </div>
+            <div className="flex flex-col items-center w-full gap-y-10">
+              <div
+                className="w-[280px] h-[252px] flex justify-center items-center bg-center bg-contain"
+                style={{ backgroundImage: `url(${wisescoreRadarSVG})` }}
+              >
+                <span className="text-color4 font-syne text-xl font-bold">
+                  {formatImpact(data?.totalScore ?? 0)}
+                </span>
+              </div>
+            </div>
+            <Button
+              className="flex items-center justify-center font-syne gap-x-1.5 h-10 w-[310px] mx-auto absolute inset-x-0 bottom-10"
+              onClick={hide}
+            >
+              Claim Privilege with WISE Credit
+              <ArrowIcon />
             </Button>
           </div>
         ),
