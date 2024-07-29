6a5bc9ce5456ef7ecf71f3a13b2b3b419cc11e12
lake <posuihushui@icloud.com>
2024-07-29 21:54:50 +0800

    update wiselevel component

--- app/campaign/src/pages/ton-wise/index.jsx
+++ app/campaign/src/pages/ton-wise/index.jsx
@@ -11,10 +11,9 @@ import Loading from '@/components/loading';
 import Generating from './generating';
 import BottomNav from './components/bottomNav';
 import { useNavigate } from 'react-router-dom';
-import WiseTag, { getWidth } from './components/wiseTag';
-import { motion } from 'framer-motion';
 import useUserInfo from '@/hooks/useUserInfoQuery';
 import LazyImage from '@/components/lazyImage';
+import WiseLevel from './components/wiseLevel';
 export default function TonWise() {
   const { user } = useUserInfo();
   const {
@@ -29,7 +28,6 @@ export default function TonWise() {
   } = useWiseGobal();
   const setShowGen = useWiseGobalMutation();
   const navigate = useNavigate();
-  const width = getWidth(totalScore);
   if (isFetching) {
     return <Loading />;
   } else if (!isGranted) {
@@ -76,26 +74,7 @@ export default function TonWise() {
                       Improve Now
                     </Link>
                   </div>
-                  <div className="space-y-2">
-                    <div className="w-full relative h-1 rounded-full bg-white/20">
-                      <motion.div
-                        className="w-10 bg-linear16 rounded-full absolute inset-y-0 bottom-0"
-                        initial={{ width: 0 }}
-                        animate={{ width }}
-                        transition={{ ease: 'easeOut', duration: 2 }}
-                      />
-                    </div>
-                    <div className="font-thin relative">
-                      <span className="text-xs text-white/80">
-                        Your WISE Level
-                      </span>
-                      <WiseTag
-                        className="text-xs absolute top-1/2 -translate-y-1/2 -translate-x-1/2"
-                        style={{ left: width }}
-                        value={totalScore}
-                      />
-                    </div>
-                  </div>
+                  <WiseLevel totalScore={totalScore} />
                 </div>
               </div>
               <Invite />
--- app/campaign/src/pages/ton-wise/wise-score.jsx
+++ app/campaign/src/pages/ton-wise/wise-score.jsx
@@ -5,15 +5,15 @@ import { lazy, Suspense } from 'react';
 
 const WiseInfo = lazy(() => import('./components/wise-info'));
 
-export default function TonWiseScore () {
+export default function TonWiseScore() {
   const { data } = useWiseScore();
 
   return (
-    <div className='px-5 mt-3 lg:px-0 max-w-md mx-auto'>
+    <div className="px-5 mt-3 lg:px-0 max-w-md mx-auto">
       {!data ? (
-        <ScoreSkeleton />
+        <Loading />
       ) : (
-        <div className='space-y-16'>
+        <div className="space-y-16">
           <Suspense fallback={<Loading />}>
             <WiseInfo />
           </Suspense>
