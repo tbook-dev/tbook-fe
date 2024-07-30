c0ca0b30cecbcccd6c44db6dfc5fd1fa57097b88
lake <posuihushui@icloud.com>
2024-07-30 15:37:24 +0800

    update new rule

--- app/campaign/src/pages/ton-wise/components/privilege.jsx
+++ app/campaign/src/pages/ton-wise/components/privilege.jsx
@@ -1,5 +1,8 @@
 import { useMemo } from 'react';
 import SBTIcon from '@/images/icon/svgr/sbt.svg?react';
+import AmbassadorIcon from '@/images/icon/svgr/ambassador.svg?react';
+import { useNavigate } from 'react-router-dom';
+import useWallet from '@/hooks/useWallet';
 
 export default function Privilege() {
   const navigate = useNavigate();
@@ -10,26 +13,40 @@ export default function Privilege() {
     return [
       {
         text: 'WISE SBT',
-        img: (
-          <Link to="/wise-score/sbt">
-            <SBTIcon className="size-[30px]" />
-          </Link>
-        ),
+        img: <SBTIcon className="size-[30px]" />,
+        handle: () => {
+          if (tonKit.connected) {
+            navigate('/wise-score/sbt');
+          } else {
+            tonKit.connectHandle();
+          }
+        },
+      },
+      {
+        text: 'Ambassador',
+        img: <AmbassadorIcon className="size-[30px]" />,
+        handle: () => {
+          navigate('/wise-score/ambassador');
+        },
       },
     ];
-  }, [tonKit]);
+  }, [tonKit, navigate]);
   return (
     <div className="space-y-2">
       <h2 className="text-sm text-white/40">Privilege</h2>
       <div className="flex items-center gap-x-2">
         {list.map((v) => {
           return (
-            <div className="space-y-1" key={v.text}>
+            <div
+              className="space-y-1 cursor-pointer"
+              key={v.text}
+              onClick={v.handle}
+            >
               <div className="size-12 rounded-full bg-white/5 flex items-center justify-center">
                 {v.img}
               </div>
               <span className="text-xs">{v.text}</span>
-            </Link>
+            </div>
           );
         })}
       </div>
