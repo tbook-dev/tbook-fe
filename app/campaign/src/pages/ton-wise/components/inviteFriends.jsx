72ac8d755963ea195ccca19a05186d5b6cda3ff8
lake <posuihushui@icloud.com>
2024-07-30 21:57:47 +0800

    update new invite code page

--- app/campaign/src/pages/ton-wise/components/inviteFriends.jsx
+++ app/campaign/src/pages/ton-wise/components/inviteFriends.jsx
@@ -4,77 +4,41 @@ import {
 } from '@/hooks/useWiseScore';
 import CopyIcon from '@/images/icon/svgr/copy.svg?react';
 import { Typography } from 'antd';
-import clsx from 'clsx';
+import TpointIcon from '@/images/icon/svgr/tpoint.svg?react';
 
 const { Paragraph } = Typography;
 
 export default function InviteFriends({ openDrawer }) {
-  const { inviteCode, totalTimes, usedTimes, invitedList } =
-    useWiseCreditInviteFriends();
+  const { inviteCode } = useWiseCreditInviteFriends();
   const { rawText } = useWiseCreditInvite();
   const hasNoData = !inviteCode;
   return (
-    <div className="py-3 px-4 bg-white/5 rounded-lg space-y-3">
-      <Paragraph
-        copyable={{
-          text: rawText,
-          icon: <CopyIcon />,
-        }}
-        className="flex justify-between gap-x-1"
-        style={{
-          marginBottom: 0,
-        }}
-      >
-        {hasNoData ? (
-          <span className="bg-[#1f1f1f] w-12 h-6 animate-pulse" />
-        ) : (
-          <span className="text-base font-medium text-white">{inviteCode}</span>
-        )}
-      </Paragraph>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center -space-x-3">
+    <div className="p-2 bg-white/5 space-y-4 rounded-xl text-center">
+      <div className="space-y-3 px-4 py-2.5 bg-white/5 rounded-xl">
+        <p className="text-sm">Invitation code</p>
+        <Paragraph
+          copyable={{
+            text: rawText,
+            icon: <CopyIcon />,
+          }}
+          className="flex justify-center items-center gap-x-1"
+          style={{
+            marginBottom: 0,
+          }}
+        >
           {hasNoData ? (
-            <span className="size-7 rounded-full bg-white/10 animate-pulse" />
+            <span className="bg-[#1f1f1f] w-12 h-9 animate-pulse" />
           ) : (
-            invitedList.map((v) => {
-              return (
-                <img
-                  src={v.avatar}
-                  className="size-7 rounded-full object-center"
-                  key={v.userId}
-                />
-              );
-            })
-          )}
-          {invitedList.length < 3 && openDrawer ? (
-            <span
-              onClick={openDrawer}
-              className={clsx(
-                'size-7 rounded-full flex justify-center items-center backdrop-blur',
-                'cursor-pointer bg-white/10'
-              )}
-            >
-              +
+            <span className="font-medium text-color8 text-3xl">
+              {inviteCode}
             </span>
-          ) : (
-            invitedList.length === 0 && (
-              <span
-                className={clsx(
-                  'size-7 rounded-full flex justify-center items-center backdrop-blur',
-                  'cursor-not-allowed bg-white/5 opacity-60'
-                )}
-              />
-            )
           )}
-        </div>
-
-        {hasNoData ? (
-          <span className="bg-[#1f1f1f] w-12 h-7 bg-white/10 animate-pulse" />
-        ) : (
-          <div className={'text-xs font-thin'}>
-            {usedTimes}/{totalTimes} Friends generated
-          </div>
-        )}
+        </Paragraph>
+      </div>
+      <div className="text-sm text-white/60">
+        Each friend generating WISE Credit Score, you'll get
+        <TpointIcon className="size-5 inline mx-1" />
+        5,000 TPoints
       </div>
     </div>
   );
