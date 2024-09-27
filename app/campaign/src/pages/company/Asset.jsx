c43cfe73459e318f2cb590cbe81b2e04aca7ae4c
karen <magic_gua@163.com>
2024-09-28 02:07:27 +0800

    feat: company assetPoints layout

--- app/campaign/src/pages/company/Asset.jsx
+++ app/campaign/src/pages/company/Asset.jsx
@@ -1,9 +1,11 @@
 import { useNavigate, useLocation, useLoaderData } from 'react-router-dom';
 
-import TabList from '../my/TabList';
+import AssetTabList from './AssetTabList';
 import Credentials from '../my/modules/Credential';
+
 import NFT from '../my/modules/NFT';
-import Point from '../my/modules/Point';
+
+import AssetPoints from './AssetPoints'
 import NotConnect from '../my/modules/NotConnect';
 
 import useUserInfoQuery from '@/hooks/useUserInfoQuery';
@@ -21,15 +23,15 @@ const tabModule = [
     value: '1',
     com: <Credentials isCompany={true} />,
   },
-  {
-    name: 'NFTs',
-    value: '2',
-    com: <NFT isCompany={true} />,
-  },
+  // {
+  //   name: 'NFTs',
+  //   value: '2',
+  //   com: <NFT isCompany={true} />,
+  // },
   {
     name: 'Points',
     value: '3',
-    com: <Point isCompany={true} />,
+    com: <AssetPoints />,
   },
 ];
 
@@ -55,20 +57,24 @@ export default function CompanyAsset ({ pageType = 'project' }) {
   
   return (
     <Layout title={ companyName || '' }>
-      <div className="mx-auto space-y-8 w-page-content pb-28">
-        <div className={ clsx("flex flex-col gap-y-4 lg:gap-y-8 pt-3 pb-4 px-4 lg:px-0 border-b lg:border-none", isLightTheme ? 'border-[#dbbee8]' : 'border-[#160b25]') }>
-          <h2 className="text-2xl font-medium lg:font-bold font-zen-dot">
+      <div className="mx-auto space-y-4 w-page-content pb-28">
+        <div className={ 
+          clsx("flex flex-col gap-y-4 lg:gap-y-8 pt-3 pb-2 px-4 lg:px-0 border-b lg:border-none", 
+          isLightTheme ? 'border-[#E8E5E9]' : 'border-[#160b25]') }>
+          <h2 className="text-xl font-bold">
             Assets
           </h2>
-          <TabList
+
+          <AssetTabList
             disabled={ !userLogined }
             tabs={ tabModule }
             value={ value }
             onSelect={ setValue }
+            className="max-w-[100px]"
           />
         </div>
 
-        <div className="px-4 lg:px-0">
+        <div className="lg:px-0">
           { userLoading ? (
             <Loading className={ clsx(isLightTheme ? 'bg-black' : 'bg-white', "z-60") } text="Aggregating metrics..." />
           ) : userLogined ? (
---
+++ app/campaign/src/pages/company/AssetTabList.jsx
@@ -0,0 +1,64 @@
+import clsx from 'clsx'
+import { useLoaderData } from 'react-router-dom'
+
+export default function AssetTabList ({ disabled, tabs, value, onSelect, className }) {
+
+  console.log('AssetTabList', tabs)
+  console.log('value', value)
+
+  const colorConfig = {
+    light: {
+      defaultTextColor: '#6D6C6E',
+      activeTextColor: '#000000',
+      bottomLineColor: '#000000',
+    },
+    dark: {
+      defaultTextColor: '#c4c4c4',
+      activeTextColor: '#904bf6',
+      bottomLineColor: '#904bf6',
+    }
+  }
+
+  const getStyles = ({ value, m, disabled }) => {
+    const { isLightTheme } = useLoaderData()
+    const theme = isLightTheme ? 'light' : 'dark';
+    const colors = colorConfig[ theme ];
+
+    const baseStyles = 'lg:px-4 lg:py-1 rounded relative lg:w-max flex-grow text-center';
+    
+    const themeStyles = `text-[${colors.defaultTextColor}]`;
+
+    const activeStyles = m.value === value
+      ? `lg:bg-[rgb(255,255,255)]/[0.1] lg:after:hidden after:absolute after:inset-x-0 after:bottom-[-8px] after:h-0.5 after:bg-[${colors.bottomLineColor}] text-[${colors.activeTextColor}]`
+      : `text-[${colors.defaultTextColor}]`;
+
+    const hoverStyles = !disabled && `hover:text-[${colors.activeTextColor}]`;
+
+    return clsx(
+      baseStyles,
+      themeStyles,
+      activeStyles,
+      hoverStyles,
+      className
+    );
+  }
+
+  return (
+    <div className='flex items-center justify-around text-md font-md lg:justify-start gap-x-6'>
+      { tabs.map(m => {
+        return (
+          <button
+            key={ m.name }
+            disabled={ disabled }
+            className={ getStyles({ value, m, disabled }) }
+            onClick={ () => {
+              onSelect(m.value)
+            } }
+          >
+            { m.name }
+          </button>
+        )
+      }) }
+    </div>
+  )
+}
